92098d0cbee2bcbd3b158ecff61d539892483238
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2020-06-18 14:22:22 +0200

    [ADD] TargetConfiguration, TargetDevice, TargetManager and Meter for HX8K development board

---
+++ adapters/icecraft_target.py
@@ -0,0 +1,295 @@
+import os
+import sys
+import random
+import struct
+from io import StringIO
+from typing import Union, NamedTuple
+
+sys.path.append("/usr/local/bin")
+import icebox
+
+sys.path.append(
+	os.path.join(
+		os.path.dirname(
+			os.path.dirname(
+				os.path.dirname(os.path.abspath(__file__))
+			)
+		),
+		"components",
+		"board"
+	)
+	
+)
+
+from fpga_board import FPGABoard
+from fpga_manager import FPGAManager
+
+from domain.interfaces import TargetDevice, Meter, TargetManager
+from domain.model import TargetConfiguration, OutputData
+from domain.request_model import RequestObject
+
+HX8K_BOARD = "ICE40HX8K-B-EVN"
+
+class TilePosition(NamedTuple):
+	x: int
+	y: int
+
+class IcecraftConfiguration(TargetConfiguration):
+	def __init__(self, ice_conf):
+		self._ice_conf = ice_conf
+	
+	def to_text(self) -> str:
+		# ice_config always opens a file to write asc to
+		# so the asc text has to be written to a file before reading its content immediately
+		# NamedTemporaryFile isn't used as it it may not be possible for ice_config to open it
+		tmp_key = "".join(random.choice("0123456789abcdef") for _ in range(6))
+		asc_name = f"tmp.to_text.{hex(random)}.asc"
+		self.write_asc(asc_name)
+		
+		with open(asc_name, "r") as asc_file:
+			asc_text = asc_file.read()
+		
+		os.remove(asc_name)
+		
+		return asc_text
+	
+	def write_asc(self, asc_name):
+		self._ice_conf.write_file(asc_name)
+	
+	def write_bitstream(self, bitstream_name):
+		asc_name = bitstream_name + ".asc"
+		self.write_asc(asc_name)
+		FPGABoard.pack_bitstream(asc_name, bitstream_name)
+		os.remove(asc_name)
+	
+	def set_ram_values(self, ram_block, address, values, mode="512x8"):
+		ram_strings = self._ice_conf.ram_data.setdefault(ram_block, ["0"*64]*16)
+		for value in values:
+			self.set_in_ram_strings(ram_strings, address, value, mode)
+			address += 1
+	
+	def get_ram_values(self, ram_block, address, count=1, mode="512x8"):
+		ram_strings = self._ice_conf.ram_data.setdefault(ram_block, ["0"*64]*16)
+		values = []
+		for tmp_address in range(address, address+count):
+			value = self.get_from_ram_strings(ram_strings, tmp_address, mode)
+			values.append(value)
+		
+		return values
+	
+	@classmethod
+	def block_size_from_mode(cls, mode):
+		return 4096//cls.value_length_from_mode(mode)
+	
+	@staticmethod
+	def value_length_from_mode(mode):
+		if (mode == 0) or (mode == "256x16"):
+			return 16
+		elif (mode == 1) or (mode == "512x8"):
+			return 8
+		elif (mode == 2) or (mode == "1024x4"):
+			return 4
+		elif (mode == 3) or (mode == "2048x2"):
+			return 2
+		else:
+			raise ValueError(f"Invalid mode: {mode}")
+	
+	@staticmethod
+	def split_address(address):
+		index = address % 256
+		offset = address // 256
+		col_index = index % 16
+		row_index = index // 16
+		
+		return row_index, col_index, offset
+	
+	@classmethod
+	def get_from_ram_strings(cls, ram_strings, address, mode="512x8"):
+		value_len = cls.value_length_from_mode(mode)
+		row_index, col_index, offset = cls.split_address(address)
+		
+		l = len(ram_strings[row_index])
+		
+		str_word = ram_strings[row_index][l-4*(col_index+1):l-4*col_index]
+		int_word = int(str_word, 16)
+		
+		if value_len == 16:
+			return int_word
+		
+		step = 16 // value_len
+		int_word >>= offset
+		mask = 1
+		value = 0
+		for i in range(value_len):
+			value |= mask & int_word
+			mask <<= 1
+			int_word >>= step - 1
+		
+		return value
+	
+	@classmethod
+	def set_in_ram_strings(cls, ram_strings, address, value, mode="512x8"):
+		value_len = cls.value_length_from_mode(mode)
+		row_index, col_index, offset = cls.split_address(address)
+		
+		assert value >= 0, "Value has to be non negative."
+		assert value < pow(2, value_len), f"Value {value} too large for bit length {value_len}."
+		
+		str_row = ram_strings[row_index]
+		
+		l = len(ram_strings[row_index])
+		
+		if value_len == 16:
+			new_int_word = value
+		else:
+			str_word = ram_strings[row_index][l-4*(col_index+1):l-4*col_index]
+			int_word = int(str_word, 16)
+			
+			step = 16 // value_len
+			new_int_word = int_word
+			mask = 1 << offset
+			value <<= offset
+			for i in range(value_len):
+				new_int_word = (new_int_word & (0xFFFF ^ mask)) | (mask & value)
+				
+				mask <<= step
+				value <<= step - 1
+		
+		new_str_word = "{:04x}".format(new_int_word)
+		assert len(new_str_word) == 4, f"Word in hex has to have 4 characters, not {len(new_str_word)}."
+		ram_strings[row_index] = str_row[:l-4*(col_index+1)] + new_str_word + str_row[l-4*col_index:]
+	
+	@classmethod
+	def create_from_file(cls, asc_filename):
+		ic = icebox.iceconfig()
+		ic.read_file(asc_filename)
+		
+		return cls(ic)
+	
+	@classmethod
+	def create_empty(cls):
+		ic = icebox.iceconfig()
+		ic.setup_empty_8k()
+		
+		return cls(ic)
+
+class IcecraftDevice(TargetDevice):
+	"""ice device"""
+	
+	def __init__(self, device: FPGABoard) -> None:
+		self._device = device
+	
+	@property
+	def serial_number(self) -> str:
+		return self._device.serial_number
+	
+	@property
+	def hardware_type(self) -> str:
+		return HX8K_BOARD
+	
+	def close(self):
+		self._device.close()
+	
+	def configure(self, configuration: TargetConfiguration) -> None:
+		# just use the configuration as if it was for the correct device
+		
+		base_name = "gen_tmp"
+		asc_name = f"{base_name}.asc"
+		bitstream_name = f"{base_name}.bin"
+		# write asc
+		configuration.write_asc(asc_name)
+		# pack asc to bin
+		self._device.pack_bitstream(asc_name, bitstream_name)
+		
+		# flash
+		self._device.flash_bitstream(bitstream_name)
+		
+		# remove asc and bin file
+		os.remove(bitstream_name)
+		os.remove(asc_name)
+	
+	def read_bytes(self, size: int) -> bytes:
+		return self._device.uart.read(size)
+	
+	def write_bytes(self, data: bytes) -> int:
+		return self._device.uart.write(data)
+
+class IcecraftManager(TargetManager):
+	"""simple management of icecraft devices without concurrency"""
+	
+	def __init__(self) -> None:
+		self._in_use = set()
+	
+	def acquire(self, serial_number: Union[str, None] = None) -> TargetDevice:
+		if serial_number is None:
+			device = FPGABoard.get_suitable_board(black_list=self._in_use)
+			serial_number = device.serial_number
+		else:
+			if serial_number in self._in_use:
+				raise ValueError(f"FPGA with serial number {serial_number} already in use")
+			device = FPGABoard(serial_number)
+		
+		self._in_use.add(serial_number)
+		
+		return IcecraftDevice(device)
+	
+	def release(self, target: TargetDevice) -> None:
+		self._in_use.remove(target.serial_number)
+		target.close()
+
+class MultiIcecraftManager(TargetManager):
+	"""management of ice devices in multiprocessing environments"""
+	
+	def __init__(self) -> None:
+		self._fpga_manager = FPGAManager.create_manager()
+	
+	def acquire(self, serial_number: Union[str, None]) -> TargetDevice:
+		raise NotImplementedError()
+	
+	def release(self, target: TargetDevice) -> None:
+		raise NotImplementedError()
+
+class IcecraftEmbedMeter(Meter):
+	"""Measure icecraft target by embedding the input data in ram"""
+	
+	@staticmethod
+	def read_data(target: TargetDevice, count: int, format_str: str) -> list:
+		size = struct.calcsize(format_str)
+		data = []
+		
+		for _ in range(count):
+			raw = target.read_bytes(size)
+			value = struct.unpack(format_str, raw)[0]
+			data.append(value)
+		
+		return data
+	
+	def __call__(self, target: TargetDevice, request: RequestObject) -> OutputData:
+		# embed input data in ram
+		config = request.configuration
+		block_size = config.block_size_from_mode(request.ram_mode)
+		start = 0
+		block_index = 0
+		while start < len(request.input_data):
+			config.set_ram_values(
+				#config.ram_coordinates(request.ram_blocks[block_index]),
+				request.ram_blocks[block_index],
+				0,
+				request.input_data[start:start+block_size],
+				request.ram_mode
+			)
+			block_index += 1
+			start += block_size
+		
+		# flash configuration
+		target.configure(config)
+		
+		# receive prefix
+		if request.prefix is not None:
+			pre = target.read_bytes(len(request.prefix))
+			assert pre == request.prefix
+		
+		# receive output data
+		data = self.read_data(target, request.output_count, request.output_format)
+		return OutputData(data)
+
---
+++ tests/test_icecraft.py
@@ -0,0 +1,251 @@
+import os
+import sys
+import unittest.mock as mock
+from dataclasses import dataclass
+from typing import List
+import unittest
+import subprocess
+import json
+import random
+
+sys.path.append("/usr/local/bin")
+import icebox
+
+from domain import model
+import domain.interfaces as interfaces
+import domain.use_cases as use_cases
+from domain.request_model import RequestObject
+import adapters.icecraft_target as icecraft
+
+TEST_DATA_DIR = __file__ + ".data"
+
+@dataclass
+class SendBRAMMeta:
+	mode: str
+	asc_filename: str
+	ram_block: icecraft.TilePosition
+	initial_data: List[int]
+	mask: int
+	
+	def __post_init__(self):
+		self.ram_block = icecraft.TilePosition(*self.ram_block)
+		self.asc_filename = os.path.join(TEST_DATA_DIR, self.asc_filename)
+
+with open(os.path.join(TEST_DATA_DIR, "send_all_bram.json"), "r") as json_file:
+	SEND_BRAM_META = tuple([SendBRAMMeta(*s) for s in json.load(json_file)])
+
+class IcecraftConfigurationTest(unittest.TestCase):
+	def setUp(self):
+		self.config_meta = {s.mode: s for s in SEND_BRAM_META}
+	
+	def test_create_empty(self):
+		config = icecraft.IcecraftConfiguration.create_empty()
+	
+	def test_create_from_file(self):
+		config = icecraft.IcecraftConfiguration.create_from_file(self.config_meta["256x16"].asc_filename)
+	
+	def test_get_ram_values(self):
+		for mode, current in self.config_meta.items():
+			with self.subTest(mode=mode):
+				config = icecraft.IcecraftConfiguration.create_from_file(current.asc_filename)
+				
+				# read single
+				for address, expected in enumerate(current.initial_data):
+					value = config.get_ram_values(current.ram_block, address, 1, mode)
+					self.assertEqual(expected, value[0])
+				
+				# read all
+				values = config.get_ram_values(current.ram_block, 0, len(current.initial_data), mode)
+				self.assertEqual(current.initial_data, values)
+	
+	def test_set_ram_values(self):
+		for mode, current in self.config_meta.items():
+			with self.subTest(mode=mode):
+				config = icecraft.IcecraftConfiguration.create_from_file(current.asc_filename)
+				
+				expected = list(current.initial_data)
+				# write single
+				for address, old_value in enumerate(current.initial_data):
+					new_value = current.mask ^ old_value
+					config.set_ram_values(current.ram_block, address, [new_value], mode)
+					expected[address] = new_value
+					values = config.get_ram_values(current.ram_block, 0, len(current.initial_data), mode)
+					self.assertEqual(expected, values)
+				
+				# write all
+				config.set_ram_values(current.ram_block, 0, current.initial_data, mode)
+				values = config.get_ram_values(current.ram_block, 0, len(current.initial_data), mode)
+				self.assertEqual(current.initial_data, values)
+	
+	def check_configuration(self, expected_config, config):
+		# compare two icebox configurations
+		for value_name in ("device", "warmboot"):
+			expected_value = getattr(expected_config, value_name)
+			given_value = getattr(config, value_name)
+			self.assertEqual(expected_value, given_value, f"Expected {value_name} to be {expected_value}, but was {given_value}.")
+		
+		for col_name in ("logic_tiles", "io_tiles", "ramb_tiles", "ramt_tiles", "ram_data", "ipcon_tiles", "symbols", "extra_bits", "dsp_tiles"):
+			expected_col = getattr(expected_config, col_name)
+			given_col = getattr(config, col_name)
+			self.assertEqual(expected_col, given_col, f"Contents of {col_name} differ from expected values:")
+	
+	def test_write_asc(self):
+		for mode, current in self.config_meta.items():
+			with self.subTest(mode=mode):
+				config = icecraft.IcecraftConfiguration.create_from_file(current.asc_filename)
+				
+				expected_ic = icebox.iceconfig()
+				expected_ic.read_file(current.asc_filename)
+				
+				tmp_filename = f"tmp.test_write_asc.{mode}.asc"
+				config.write_asc(tmp_filename)
+				
+				ic = icebox.iceconfig()
+				ic.read_file(tmp_filename)
+				
+				os.remove(tmp_filename)
+				
+				self.check_configuration(expected_ic, ic)
+	
+	def test_write_bitstream(self):
+		for mode, current in self.config_meta.items():
+			with self.subTest(mode=mode):
+				config = icecraft.IcecraftConfiguration.create_from_file(current.asc_filename)
+				
+				expected_ic = icebox.iceconfig()
+				expected_ic.read_file(current.asc_filename)
+				
+				tmp_bin = f"tmp.test_write_bitstream.{mode}.bin"
+				tmp_asc = f"tmp.test_write_bitstream.{mode}.asc"
+				config.write_bitstream(tmp_bin)
+				
+				subprocess.run(
+					["iceunpack", tmp_bin, tmp_asc],
+					stdin=subprocess.DEVNULL,
+					stderr=subprocess.STDOUT,
+					#text=True, # only from Python version 3.7 on
+					universal_newlines=True
+				)
+				
+				ic = icebox.iceconfig()
+				ic.read_file(tmp_asc)
+				
+				os.remove(tmp_bin)
+				os.remove(tmp_asc)
+				
+				self.check_configuration(expected_ic, ic)
+		
+
+class IcecraftDeviceTest(unittest.TestCase):
+	def get_configured_device(self, asc_filename):
+		fpga = icecraft.FPGABoard.get_suitable_board()
+		device = icecraft.IcecraftDevice(fpga)
+		config = icecraft.IcecraftConfiguration.create_from_file(asc_filename)
+		device.configure(config)
+		
+		return device
+	
+	@unittest.skipIf(len(icecraft.FPGABoard.get_suitable_serial_numbers()) < 1, "no hardware")
+	def test_creation(self):
+		fpga = icecraft.FPGABoard.get_suitable_board()
+		device = icecraft.IcecraftDevice(fpga)
+		
+		device.close()
+	
+	@unittest.skipIf(len(icecraft.FPGABoard.get_suitable_serial_numbers()) < 1, "no hardware")
+	def test_serial_number(self):
+		fpga = icecraft.FPGABoard.get_suitable_board()
+		exp_sn = fpga.serial_number
+		device = icecraft.IcecraftDevice(fpga)
+		sn = device.serial_number
+		self.assertEqual(exp_sn, sn)
+		
+		device.close()
+	
+	@unittest.skipIf(len(icecraft.FPGABoard.get_suitable_serial_numbers()) < 1, "no hardware")
+	def test_hardware_type(self):
+		fpga = icecraft.FPGABoard.get_suitable_board()
+		device = icecraft.IcecraftDevice(fpga)
+		exp_ht = icecraft.HX8K_BOARD
+		ht = device.hardware_type
+		self.assertEqual(exp_ht, ht)
+		
+		device.close()
+	
+	@unittest.skipIf(len(icecraft.FPGABoard.get_suitable_serial_numbers()) < 1, "no hardware")
+	def test_configure(self):
+		device = self.get_configured_device(SEND_BRAM_META[0].asc_filename)
+		
+		device.close()
+	
+	@unittest.skipIf(len(icecraft.FPGABoard.get_suitable_serial_numbers()) < 1, "no hardware")
+	def test_read_bytes(self):
+		meta = next(s for s in SEND_BRAM_META if s.mode=="512x8")
+		device = self.get_configured_device(meta.asc_filename)
+		
+		exp_values = bytes(meta.initial_data)
+		values = device.read_bytes(len(exp_values))
+		self.assertEqual(exp_values, values)
+		
+		device.close()
+	
+	@unittest.skipIf(len(icecraft.FPGABoard.get_suitable_serial_numbers()) < 1, "no hardware")
+	def test_write_bytes(self):
+		asc_filename = os.path.join(TEST_DATA_DIR, "echo.asc")
+		
+		device = self.get_configured_device(asc_filename)
+		for data in (bytes([0]), bytes([1]), bytes([255, 0])):
+			device.write_bytes(data)
+			recv = device.read_bytes(len(data))
+			
+			self.assertEqual(data, recv)
+			#print(data, recv)
+		
+		device.close()
+	
+
+class IcecraftManagerTest(unittest.TestCase):
+	@unittest.skipIf(len(icecraft.FPGABoard.get_suitable_serial_numbers()) < 1, "no hardware")
+	def test_no_double_acquire(self):
+		manager = icecraft.IcecraftManager()
+		device = manager.acquire()
+		with self.assertRaises(ValueError):
+			device2 = manager.acquire(serial_number=device.serial_number)
+	
+	@unittest.skipIf(len(icecraft.FPGABoard.get_suitable_serial_numbers()) < 1, "no hardware")
+	def test_no_double_release(self):
+		manager = icecraft.IcecraftManager()
+		device = manager.acquire()
+		manager.release(device)
+		with self.assertRaises(KeyError):
+			manager.release(device)
+	
+
+class IcecraftEmbedMeterTest(unittest.TestCase):
+	@unittest.skipIf(len(icecraft.FPGABoard.get_suitable_serial_numbers()) < 1, "no hardware")
+	def test_call(self):
+		format_dict = {"256x16": "<H", "512x8": "B", "1024x4": "B", "2048x2": "B"}
+		meter = icecraft.IcecraftEmbedMeter()
+		fpga = icecraft.FPGABoard.get_suitable_board()
+		device = icecraft.IcecraftDevice(fpga)
+		
+		for send_meta in SEND_BRAM_META:
+			with self.subTest(mode=send_meta.mode):
+				config = icecraft.IcecraftConfiguration.create_from_file(send_meta.asc_filename)
+				# address 0 is always 0, even if init values tries to define it otherwise
+				data = model.InputData([
+					random.randint(0, send_meta.mask) for _ in range(config.block_size_from_mode(send_meta.mode))
+				])
+				req = RequestObject()
+				req["configuration"] = config
+				req["ram_mode"] = send_meta.mode
+				req["ram_blocks"] = [send_meta.ram_block]
+				req["input_data"] = data
+				req["prefix"] = None
+				req["output_count"] = len(data)
+				req["output_format"] = format_dict[req.ram_mode]
+				
+				output = meter(device, req)
+				self.assertEqual(data, output)
+		
+		device.close()
