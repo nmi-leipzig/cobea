39bc7f9e7f8b556edde1e635d7e94189b2be6777
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2020-06-18 19:09:14 +0200

    [ADD] PreprocessingLibrary based on SciPy

---
+++ adapters/scipy_preprocessing.py
@@ -0,0 +1,32 @@
+from typing import Tuple
+from functools import partial
+
+import numpy as np
+from scipy.stats import stats
+
+from domain.interfaces import PreprocessingLibrary
+from domain.model import PreprocessingImpl, InputData, OutputData
+from domain.request_model import RequestObject, ParameterValues
+
+class SciPyPreprocessing(PreprocessingLibrary):
+	# preprocessing identifier -> parameters required
+	param_dict = {
+		"select": ("start", "end"),
+	}
+	
+	def __init__(self):
+		pass
+	
+	def get_implementation(self, request: RequestObject) -> PreprocessingImpl:
+		func = getattr(self, request.identifier)
+		params = ParameterValues()
+		for name in self.param_dict[request.identifier]:
+			params[name] = request[name]
+		return partial(func, params=params)
+	
+	@staticmethod
+	def select(input_data: InputData, output_data: OutputData, params: ParameterValues) -> Tuple[InputData, OutputData]:
+		new_in = InputData(input_data[params.start:params.end])
+		new_out = OutputData(output_data[params.start:params.end])
+		
+		return new_in, new_out
--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -6,6 +6,7 @@ import sys
 import unittest
 
 from adapters.scipy_functions import SciPyFunctions
+from adapters.scipy_preprocessing import SciPyPreprocessing
 import domain.model as model
 import domain.interfaces as interfaces
 import domain.use_cases as use_cases
@@ -15,17 +16,17 @@ class SciPyFunctionsTest(unittest.TestCase):
 		pass
 	
 	def test_creation(self):
-		scp = SciPyFunctions()
+		spf = SciPyFunctions()
 	
 	def test_get_implementation(self):
-		scp = SciPyFunctions()
-		res = scp.get_implementation("pearsons_correlation")
+		spf = SciPyFunctions()
+		res = spf.get_implementation("pearsons_correlation")
 		
 		self.assertEqual(SciPyFunctions.pearsons_correlation, res)
 	
 	def test_use_case_creation(self):
-		scp = SciPyFunctions()
-		create_case = use_cases.CreateFitnessFunction(scp)
+		spf = SciPyFunctions()
+		create_case = use_cases.CreateFitnessFunction(spf)
 		req = use_cases.RequestObject()
 		identifier = "pearsons_correlation"
 		description = "Pearson Correlation by SciPy"
@@ -38,8 +39,8 @@ class SciPyFunctionsTest(unittest.TestCase):
 		self.assertEqual(SciPyFunctions.pearsons_correlation, fitness_function.implementation)
 	
 	def test_function(self):
-		scp = SciPyFunctions()
-		create_case = use_cases.CreateFitnessFunction(scp)
+		spf = SciPyFunctions()
+		create_case = use_cases.CreateFitnessFunction(spf)
 		fitness_function = create_case(use_cases.RequestObject(
 			identifier = "pearsons_correlation",
 			description = "Pearson Correlation by SciPy"
@@ -51,3 +52,43 @@ class SciPyFunctionsTest(unittest.TestCase):
 		expected = SciPyFunctions.pearsons_correlation(test_in, test_out)
 		res = fitness_function(test_in, test_out)
 		self.assertEqual(expected, res)
+
+class SciPyPreprocessingTest(unittest.TestCase):
+	def get_example_request(self):
+		return use_cases.RequestObject(
+			identifier = "select",
+			description = "select [3:5]",
+			start = 3,
+			end = 5
+		)
+	
+	def check_example(self, func):
+		org_in = model.InputData(range(10))
+		org_out = model.OutputData(range(10,20))
+		
+		res_in, res_out = func(org_in, org_out)
+		
+		self.assertEqual(org_in[3:5], res_in)
+		self.assertEqual(org_out[3:5], res_out)
+		
+		self.assertTrue(isinstance(res_in, model.InputData))
+		self.assertTrue(isinstance(res_out, model.OutputData))
+	
+	def test_creation(self):
+		spp = SciPyPreprocessing()
+	
+	def test_get_implementation(self):
+		spp = SciPyPreprocessing()
+		
+		res = spp.get_implementation(self.get_example_request())
+		self.check_example(res)
+	
+	def test_create_use_case(self):
+		spp = SciPyPreprocessing()
+		use_case = use_cases.CreatePreprocessing(spp)
+		req = self.get_example_request()
+		
+		res = use_case(req)
+		self.assertEqual(req.identifier, res.identifier)
+		self.assertEqual(req.description, res.description)
+		self.check_example(res)
