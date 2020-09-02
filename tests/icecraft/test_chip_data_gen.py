d4c4db7961bbbb3d3a11232b11722b5a2d054dcf
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2020-09-02 19:04:50 +0200

    [ADD] test for function that fixes known issues with segments from iceconfig

--- tests/icecraft/test_chip_data_gen.py
+++ tests/icecraft/test_chip_data_gen.py
@@ -1,5 +1,9 @@
 import unittest
 import copy
+import sys
+
+sys.path.append("/usr/local/bin")
+import icebox
 
 import adapters.icecraft.chip_data_gen as chip_data_gen
 from adapters.icecraft.chip_data_utils import get_segs_for_tile
@@ -62,3 +66,73 @@ class ChipDataGenTest(unittest.TestCase):
 		# check consistence
 		self.assertEqual(set(self.org_seg_kinds), set(srt_seg_kinds))
 		self.check_segments(srt_seg_kinds, srt_tile_map, self.all_segs)
+	
+	def test_fix_known_issues(self):
+		# description, in_segs, exp_segs
+		test_data = (
+			(
+				"glb_netwk",
+				[
+					((0, 16, 'glb_netwk_1'), (0, 16, 'padin_1'), (0, 17, 'glb_netwk_1'), (0, 18, 'glb_netwk_1'), (17, 33, 'fabout')),
+					((33, 14, 'glb_netwk_0'), (33, 15, 'glb_netwk_0'), (33, 16, 'glb_netwk_0')),
+				],
+				[
+					((0, 16, 'glb_netwk_1'), (0, 16, 'padin_1'), (0, 17, 'glb_netwk_1'), (0, 18, 'glb_netwk_1'), (17, 33, 'fabout')),
+					((17, 0, 'fabout'), (33, 14, 'glb_netwk_0'), (33, 15, 'glb_netwk_0'), (33, 16, 'glb_netwk_0'), (33, 16, 'padin_1')),
+				]
+			),
+			(
+				"RAM neigh_op_top/bot",
+				[
+					(
+						(7, 29, 'neigh_op_tnr_3'), (7, 30, 'neigh_op_rgt_3'), (7, 31, 'neigh_op_bnr_3'),
+						(8, 29, 'neigh_op_top_3'), (8, 30, 'ram/RDATA_4'), (8, 31, 'neigh_op_bot_3'),
+						(9, 29, 'neigh_op_tnl_3'), (9, 30, 'neigh_op_lft_3'), (9, 31, 'neigh_op_bnl_3')
+					),
+					(
+						(7, 1, 'neigh_op_bnr_1'), (7, 1, 'neigh_op_bnr_5'), (8, 0, 'io_0/D_IN_1'),
+						(8, 1, 'neigh_op_bot_1'), (8, 1, 'neigh_op_bot_5'), (9, 1, 'neigh_op_bnl_1'),
+						(9, 1, 'neigh_op_bnl_5')
+					),
+				],
+				[
+					(
+						(7, 29, 'neigh_op_tnr_3'), (7, 30, 'neigh_op_rgt_3'), (7, 31, 'neigh_op_bnr_3'),
+						(8, 30, 'ram/RDATA_4'),
+						(9, 29, 'neigh_op_tnl_3'), (9, 30, 'neigh_op_lft_3'), (9, 31, 'neigh_op_bnl_3')
+					),
+					(
+						(7, 1, 'neigh_op_bnr_1'), (7, 1, 'neigh_op_bnr_5'), (8, 0, 'io_0/D_IN_1'),
+						(9, 1, 'neigh_op_bnl_1'), (9, 1, 'neigh_op_bnl_5')
+					),
+				]
+			),
+			(
+				"io_global/latch",
+				[
+					((0, 15, 'fabout'), (0, 32, 'io_global/latch')),
+				],
+				[
+					(
+						(0, 1, 'io_global/latch'), (0, 2, 'io_global/latch'), (0, 3, 'io_global/latch'), (0, 4, 'io_global/latch'),
+						(0, 5, 'io_global/latch'), (0, 6, 'io_global/latch'), (0, 7, 'io_global/latch'), (0, 8, 'io_global/latch'),
+						(0, 9, 'io_global/latch'), (0, 10, 'io_global/latch'), (0, 11, 'io_global/latch'), (0, 12, 'io_global/latch'),
+						(0, 13, 'io_global/latch'), (0, 14, 'io_global/latch'), (0, 15, 'fabout'), (0, 15, 'io_global/latch'),
+						(0, 16, 'io_global/latch'), (0, 17, 'io_global/latch'), (0, 18, 'io_global/latch'), (0, 19, 'io_global/latch'),
+						(0, 20, 'io_global/latch'), (0, 21, 'io_global/latch'), (0, 22, 'io_global/latch'), (0, 23, 'io_global/latch'),
+						(0, 24, 'io_global/latch'), (0, 25, 'io_global/latch'), (0, 26, 'io_global/latch'), (0, 27, 'io_global/latch'),
+						(0, 28, 'io_global/latch'), (0, 29, 'io_global/latch'), (0, 30, 'io_global/latch'), (0, 31, 'io_global/latch'),
+						(0, 32, 'io_global/latch')
+					),
+				]
+			)
+		)
+		
+		ic = icebox.iceconfig()
+		ic.setup_empty_8k()
+		
+		for desc, in_segs, exp_segs in test_data:
+			with self.subTest(desc=desc):
+				out_segs = chip_data_gen.fix_known_issues(ic, in_segs)
+				
+				self.assertEqual(exp_segs, out_segs)
