6d1c066dd1f5618c69fb10e6efe9fcc48d93bb56
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2020-09-17 19:20:56 +0200

    [CHANGE] move code to get segment from seg kind to separate function

--- adapters/icecraft/chip_data_utils.py
+++ adapters/icecraft/chip_data_utils.py
@@ -11,13 +11,25 @@ ConfigKindType = NewType("ConfigKindType", Tuple[ConfigEntryType])
 BitType = NewType("BitType", Tuple[int, int])
 DriverType = NewType("DriverType", Tuple[bool, Tuple[int, ...]])
 
+def seg_from_seg_kind(seg_kind: SegType, tile_pos: TileType, role: int) -> SegType:
+	"""reconstruct segment from segment kind, tile position and role"""
+	x_off = tile_pos[0] - seg_kind[role][0]
+	y_off = tile_pos[1] - seg_kind[role][1]
+	seg = tuple((x+x_off, y+y_off, n) for x, y, n in seg_kind)
+	
+	return seg
+
 def get_segs_for_tile(seg_kinds: List[SegType], tile_pos: TileType, seg_refs: Iterable[SegRefType]) -> List[SegType]:
+	"""Compute segments of a tile from segment kinds and segment references
+	
+	a segment reference consists of an index of the segment kind and 
+	an index inside the segment kind to define the role of the specific
+	tile in the segment
+	"""
 	segs = []
 	for seg_index, role in seg_refs:
 		seg_kind = seg_kinds[seg_index]
-		x_off = tile_pos[0] - seg_kind[role][0]
-		y_off = tile_pos[1] - seg_kind[role][1]
-		seg = tuple((x+x_off, y+y_off, n) for x, y, n in seg_kind)
+		seg = seg_from_seg_kind(seg_kind, tile_pos, role)
 		segs.append(seg)
 	
 	return segs
--- tests/icecraft/test_chip_data_utils.py
+++ tests/icecraft/test_chip_data_utils.py
@@ -3,20 +3,28 @@ import unittest
 import adapters.icecraft.chip_data_utils as chip_data_utils
 
 class ChipDataUtilsTest(unittest.TestCase):
+	# test data
+	tile_pos = (1, 2)
+	seg_refs = [(0, 2), (2, 0)]
+	seg_kinds = (
+		((0, 0, "sp_v_0"), (0, 1, "sp_v_1"), (0, 2, "sp_v_2")),
+		((0, 0, "sp_h_0"), (1, 0, "sp_h_1"), (2, 0, "sp_h_2")),
+		((0, 0, "internal"), ),
+	)
+	segs = (
+		((1, 0, "sp_v_0"), (1, 1, "sp_v_1"), (1, 2, "sp_v_2")),
+		((1, 2, "internal"), )
+	)
+	
+	def test_seg_from_seg_kind(self):
+		for (kind_index, role), exp in zip(self.seg_refs, self.segs):
+			kind = self.seg_kinds[kind_index]
+			with self.subTest(role=role, kind=kind):
+				res = chip_data_utils.seg_from_seg_kind(kind, self.tile_pos, role)
+				self.assertEqual(exp, res)
 	
 	def test_get_segs_for_tile(self):
-		tile_pos = (1, 2)
-		seg_refs = [(0, 2), (2, 0)]
-		seg_kinds = (
-			((0, 0, "sp_v_0"), (0, 1, "sp_v_1"), (0, 2, "sp_v_2")),
-			((0, 0, "sp_h_0"), (1, 0, "sp_h_1"), (2, 0, "sp_h_2")),
-			((0, 0, "internal"), ),
-		)
-		segs = set((
-			((1, 0, "sp_v_0"), (1, 1, "sp_v_1"), (1, 2, "sp_v_2")),
-			((1, 2, "internal"), )
-		))
 		
-		res = chip_data_utils.get_segs_for_tile(seg_kinds, tile_pos, seg_refs)
-		self.assertEqual(segs, set(res))
+		res = chip_data_utils.get_segs_for_tile(self.seg_kinds, self.tile_pos, self.seg_refs)
+		self.assertEqual(set(self.segs), set(res))
 
