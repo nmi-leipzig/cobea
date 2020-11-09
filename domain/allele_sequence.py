5d4b5ae552d9c3a769e33f48930e4c3f3833b159
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2020-11-09 17:04:32 +0100

    [ADD] AlleleSequence that represents LUTs with unused inputs

--- domain/allele_sequence.py
+++ domain/allele_sequence.py
@@ -3,7 +3,7 @@
 import collections
 import math
 from dataclasses import dataclass, field
-from typing import Iterable, Tuple, Any
+from typing import Iterable, Tuple, Any, List
 
 @dataclass(frozen=True)
 class Allele:
@@ -67,7 +67,7 @@ class AlleleList(AlleleSequence):
 		return True
 
 class AlleleAll(AlleleSequence):
-	"""all possible combinations of the list"""
+	"""all possible combinations of bit values"""
 	
 	def __init__(self, bit_count: int) -> None:
 		self._bit_count = bit_count
@@ -116,3 +116,137 @@ class AlleleAll(AlleleSequence):
 				index |= 1
 		
 		return index
+
+class AllelePow(AlleleSequence):
+	"""all possible output combinations of a LUT with used and unused inputs
+	
+	A LUT with i inputs has 2^i possible outputs (input combinations), which corresponds to
+	the number of bits. Therefore there are 2^2^i possible combinations of
+	outputs (i.e. sequence items).
+	
+	The output is independent from unused inputs. Hence if u inputs are unused,
+	the number of bits remains 2^i, but the number of possible outputs
+	reduces to 2^(i-u) and the number of possible combinations of outputs to 2^2^(i-u).
+	"""
+	
+	def __init__(self, input_count: int, unused_inputs: Iterable[int]) -> None:
+		if len(set(unused_inputs)) != len(unused_inputs):
+			raise ValueError(f"unused input entered multiple times: {unused_inputs}")
+		for u in unused_inputs:
+			if u >= input_count:
+				raise ValueError(f"input {u} not in inputs [0..{input_count-1}]")
+		
+		self._input_count = input_count
+		self._unused = tuple(sorted(unused_inputs))
+		self._used = tuple(i for i in range(input_count) if i not in self._unused)
+		self._pos_outputs = pow(2, self._input_count-len(self._unused))
+		self._output_map = self._create_output_map()
+	
+	def _create_output_map(self) -> List[Tuple[int, ...]]:
+		"""create map from the bit of an AlleleSequence index (aka key)
+		to the bits in an allele that have the same value
+		"""
+		output_map = []
+		
+		# create all combinations of unused inputs
+		unused_combs = [self.insert_bits(i, self._used) for i in range(pow(2, len(self._unused)))]
+		
+		# iterate over possible outputs
+		for in_comb in range(self._pos_outputs):
+			# insert bits from unused outputs
+			used_base = self.insert_bits(in_comb, self._unused)
+			same_out = tuple(used_base|u for u in unused_combs)
+			output_map.append(same_out)
+		
+		# as the index is sorted by lowest index last, while tuples of
+		# bool are ordered by lowest index first, the index has to be reversed
+		output_map.reverse()
+		return output_map
+	
+	def __len__(self) -> int:
+		return pow(2, self._pos_outputs)
+	
+	def __getitem__(self, key: int) -> Allele:
+		if key >= len(self) or key < 0:
+			raise IndexError()
+		
+		tmp = [None] * (1 << self._input_count)
+		index = key
+		for i, out_bits in enumerate(self._output_map):
+			bit_val = key%2==1
+			key //= 2
+			
+			for bit_index in out_bits:
+				tmp[bit_index] = bit_val
+		
+		return Allele(tuple(tmp), "{}".format(index))
+	
+	def __eq__(self, other: Any) -> bool:
+		if isinstance(other, self.__class__):
+			return self._input_count == other._input_count and self._unused == other._unused
+		else:
+			return NotImplemented
+	
+	def __hash__(self):
+		return hash((self._input_count, self._unused))
+	
+	def size_in_bits(self) -> float:
+		return float(self._pos_outputs)
+	
+	def values_index(self, values: Iterable[bool]) -> int:
+		index = 0
+		for out_bits in reversed(self._output_map):
+			index <<= 1
+			value = values[out_bits[0]]
+			
+			for bit_index in out_bits:
+				if values[bit_index] != value:
+					raise ValueError(f"No allele with bit values {values}: is {value} at {out_bits[0]}, but {values[bit_index]} at {bit_index}")
+			
+			if value:
+				index |= 1
+		
+		return index
+	
+	@staticmethod
+	def delete_bit(value, bit_index):
+		"""delete a single bit from an integer"""
+		prefix = value & ((1 << bit_index) - 1)
+		suffix = value ^ (value & ((1 << bit_index + 1) - 1))
+		
+		return (suffix >> 1) | prefix
+	
+	@classmethod
+	def delete_bits(cls, value, bit_indices):
+		"""delete multiple bits from an integer
+		
+		the bit indices are noted with respect to the original value
+		e.g.: value = 0b01101010, bit_indices=(0, 2, 4) -> 0b00001111
+		"""
+		for offset, bit_index in enumerate(sorted(bit_indices)):
+			value = cls.delete_bit(value, bit_index - offset)
+		return value
+	
+	@staticmethod
+	def insert_bit(value, bit_index):
+		"""insert single bit in integer value
+		
+		the new bit is reset (i.e. 0)
+		"""
+		prefix = value & ((1 << bit_index) - 1)
+		suffix = value ^ prefix
+		
+		return (suffix << 1) | prefix
+	
+	@classmethod
+	def insert_bits(cls, value, bit_indices):
+		"""insert multiple bits in integer value
+		
+		the bit indices are the inidices of the new bits after every bit was inserted
+		e.g.: value=0b00001111, bit_indices=(0, 2, 4) -> 0b01101010
+		"""
+		for bit_index in bit_indices:
+			value = cls.insert_bit(value, bit_index)
+		
+		return value
+	
--- tests/test_allele_sequence.py
+++ tests/test_allele_sequence.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import unittest
+import math
+from typing import NamedTuple, Tuple, List
 
-from domain.allele_sequence import Allele, AlleleAll, AlleleList
+from domain.allele_sequence import Allele, AlleleAll, AlleleList, AllelePow
 
 class AlleleTest(unittest.TestCase):
 	def test_eq(self):
@@ -124,4 +126,213 @@ class AlleleAllTest(unittest.TestCase):
 				alleles, exp_alleles = self.create(bit_count)
 				with self.assertRaises(ValueError):
 					alleles.values_index((True, )*(bit_count-1))
+	
+
+class AllelePowTest(unittest.TestCase):
+	class AllelePowTestData(NamedTuple):
+		desc: str
+		input_count: int
+		unused_inputs: Tuple[int, ...]
+		alleles: Tuple[Allele, ...]
+		output_map: List[Tuple[int, ...]]
+		
+	
+	test_cases = (
+		AllelePowTestData(
+			"no unused inputs, 1 input", 1, tuple(),
+			(
+				Allele((False, False), "0"),
+				Allele((False, True), "1"),
+				Allele((True, False), "2"),
+				Allele((True, True), "3"),
+			),
+			[(1, ), (0, )]
+		),
+		AllelePowTestData(
+			"all inputs unused, 1 input", 1, (0, ),
+			(
+				Allele((False, False), "const 0"),
+				Allele((True, True), "const 1"),
+			),
+			[(0, 1)]
+		),
+		AllelePowTestData(
+			"all inputs unused, 3 inputs", 3, (0, 1, 2),
+			(
+				Allele((False, )*8, "const 0"),
+				Allele((True, )*8, "const 1"),
+			),
+			[(0, 1, 2, 3, 4, 5, 6, 7)]
+		),
+		AllelePowTestData(
+			"two inputs, first unused", 2, (0, ),
+			(
+				Allele((False, False, False, False), "0"),
+				Allele((False, False, True, True), "1"),
+				Allele((True, True, False, False), "2"),
+				Allele((True, True, True, True), "3"),
+			),
+			[(2, 3), (0, 1)]
+		),
+		AllelePowTestData(
+			"two inputs, second unused", 2, (1, ),
+			(
+				Allele((False, False, False, False), "0"),
+				Allele((False, True, False, True), "1"),
+				Allele((True, False, True, False), "2"),
+				Allele((True, True, True, True), "3"),
+			),
+			[(1, 3), (0, 2)]
+		),
+	)
+	
+	def create_instance(self, test_case):
+		return AllelePow(test_case.input_count, test_case.unused_inputs)
+	
+	def test_creation(self):
+		for test_case in self.test_cases:
+			with self.subTest(desc=test_case.desc):
+				self.create_instance(test_case)
+	
+	def test_output_map(self):
+		for test_case in self.test_cases:
+			with self.subTest(desc=test_case.desc):
+				dut = self.create_instance(test_case)
+				
+				# plausibility check
+				len_sum = 0
+				full_set = set()
+				for s in dut._output_map:
+					len_sum += len(s)
+					full_set.update(s)
+				
+				self.assertEqual(len_sum, len(full_set), "Inconsistent length; Doublicated values?")
+				self.assertEqual(set(range(pow(2, test_case.input_count))), full_set)
+				
+				self.assertEqual(test_case.output_map, dut._output_map)
+	
+	def test_len(self):
+		for test_case in self.test_cases:
+			with self.subTest(desc=test_case.desc):
+				dut = self.create_instance(test_case)
+				exp_len = len(test_case.alleles)
+				
+				self.assertEqual(exp_len, len(dut))
+	
+	def test_getitem(self):
+		for test_case in self.test_cases:
+			with self.subTest(desc=test_case.desc):
+				dut = self.create_instance(test_case)
+				
+				for index, exp_allele in enumerate(test_case.alleles):
+					self.assertEqual(exp_allele, dut[index])
+	
+	def test_values_index(self):
+		for test_case in self.test_cases:
+			with self.subTest(desc=test_case.desc):
+				dut = self.create_instance(test_case)
+				
+				for exp_index, allele in enumerate(test_case.alleles):
+					self.assertEqual(exp_index, dut.values_index(allele.values))
+	
+	def check_eq(self, a, b, exp):
+		res = (a == b)
+		self.assertEqual(exp, res)
+		res = (b == a)
+		self.assertEqual(exp, res)
+	
+	def test_eq(self):
+		for tc_a in self.test_cases:
+			for tc_b in self.test_cases:
+				with self.subTest(desc=f"'{tc_a.desc}' == '{tc_b.desc}'"):
+					a = self.create_instance(tc_a)
+					b = self.create_instance(tc_b)
+					exp = (tc_a == tc_b)
+					
+					self.check_eq(a, b, exp)
+		
+		for i in range(1, 5):
+			with self.subTest(desc=f"AlleleAll == AllelePow for {i}"):
+				dut_1 = AlleleAll(i)
+				dut_2 = AllelePow(i, [])
+				
+				self.check_eq(dut_1, dut_2, False)
+	
+	def test_size_in_bits(self):
+		for test_case in self.test_cases:
+			with self.subTest(desc=test_case.desc):
+				dut = self.create_instance(test_case)
+				exp_len = math.log2(len(test_case.alleles))
+				
+				self.assertEqual(exp_len, dut.size_in_bits())
+		
+	
+	def generic_delete_bit_test(self, delete_func):
+		test_cases = (
+			(0b00000000, 3, 0b00000000),
+			(0b11111111, 4, 0b01111111),
+			(0b11001100, 0, 0b01100110),
+			(0b11001100, 3, 0b01100100),
+			(0b11001100, 10, 0b11001100),
+			(0b1101101001011000, 10, 0b0110111001011000),
+		)
+		
+		for value, bit_index, exp in test_cases:
+			with self.subTest(desc=f"delete bit {bit_index:2d} of {value:08b}"):
+				res = delete_func(value, bit_index)
+				self.assertEqual(exp, res, f"{exp:08b} != {res:08b}")
+	
+	def test_delete_bit(self):
+		self.generic_delete_bit_test(AllelePow.delete_bit)
+	
+	def generic_delete_bits(self, delete_func):
+		test_cases = (
+			(0b00000000, (0, 3), 0b00000000),
+			(0b01001110, (4, ), 0b00101110),
+			(0b01001110, (1, 3), 0b00010010),
+			(0b11110101, (2, 4, 6), 0b00011001),
+		)
+		
+		for value, bit_indices, exp in test_cases:
+			with self.subTest(desc=f"delete bits {bit_indices} of {value:08b}"):
+				res = delete_func(value, bit_indices)
+				self.assertEqual(exp, res, f"{exp:08b} != {res:08b}")
+	
+	def test_delete_bits(self):
+		self.generic_delete_bits(AllelePow.delete_bits)
+	
+	def generic_insert_bit_test(self, insert_func):
+		test_cases = (
+			(0b00000000, 3, 0b00000000),
+			(0b01111111, 4, 0b11101111),
+			(0b01100110, 0, 0b11001100),
+			(0b01100100, 3, 0b11000100),
+			(0b11001100, 10, 0b11001100),
+			(0b0110111001011000, 10, 0b1101101001011000),
+		)
+		
+		for value, bit_index, exp in test_cases:
+			with self.subTest(value=value, bit_index=bit_index):
+				res = insert_func(value, bit_index)
+				self.assertEqual(exp, res, f"{exp:08b} != {res:08b}")
+	
+	def test_insert_bit(self):
+		self.generic_insert_bit_test(AllelePow.insert_bit)
+	
+	def generic_insert_bits_test(self, insert_func):
+		test_cases = (
+			(0b00000000, (0, 3), 0b00000000),
+			(0b00101110, (4, ), 0b01001110),
+			(0b00010010, (1, 3), 0b01000100),
+			(0b00011001, (2, 4, 6), 0b10100001),
+		)
+		
+		for value, bit_indices, exp in test_cases:
+			with self.subTest(value=value, bit_indices=bit_indices):
+				res = insert_func(value, bit_indices)
+				self.assertEqual(exp, res, f"{exp:08b} != {res:08b}")
+		
+	
+	def test_insert_bits(self):
+		self.generic_insert_bits_test(AllelePow.insert_bits)
 	
