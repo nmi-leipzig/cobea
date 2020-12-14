7cb00cc81d5468703a4088f9dd4aa8a8c0543704
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2020-12-14 16:55:41 +0100

    [ADD] comment to describe approach for creating icecraft representation

--- adapters/icecraft/representation.py
+++ adapters/icecraft/representation.py
@@ -267,6 +267,20 @@ class IcecraftRep(Representation):
 	
 
 class IcecraftRepGen(RepresentationGenerator):
+	"""Generate a representation for ice40 FPGAs
+	
+	The information about the FPGA ressources (nets, connections, LUTs) is transformed to internal 
+	data structures, a request is parsed to select which ressources belong to the phenotype and 
+	therefore should be represented in the genotype.
+	
+	A coarse selection is done by selcting the tiles, by default all resoources in the selected tiles are represented.
+	Ressources can be excluded in two ways:
+	- not available -> ressource doesn't belong to the genotype nor the phenotype and is not 
+	configured, i.e. bits will not be set; described by the request
+	 unused -> ressource will not be used have a fixed, neutral genotype and phneotype, e.g bits 
+	will be constantly set to neutral (as a general rule to 0); described by (the negation of) 
+	a used function constructed from the request
+	"""
 	def __init__(self) -> None:
 		self._parameters = {"__call__": [
 			Parameter("x_min", int),
