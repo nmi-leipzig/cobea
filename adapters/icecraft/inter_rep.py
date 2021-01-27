2a1ed415bd84f7f6b0c2d1eb497f8faf67250ce6
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2021-01-27 10:02:46 +0100

    [ADD] post init check for EdgeDesig; [CHANGE] InterElement dataclasses are not frozen; [ADD] dataclass for source groups; [ADD] dataclass for data about edges; [ADD] dataclass common to all vertices; [ADD] dataclass for connection vertices; [ADD] dataclass for LUT vertices; [CHANGE] pass config_map to InterRep init; [CHANGE] add connectio and LUT vertices in InterRep init; [ADD] functions to add edges and vertices to InterRep; [ADD] functions to iterate all vertices and all edges in InterRep

--- adapters/icecraft/inter_rep.py
+++ adapters/icecraft/inter_rep.py
@@ -19,16 +19,19 @@ Multiedges are possible, if a net has two different names that both can
 drive the same net.
 """
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import total_ordering
-from typing import Iterable, Union, Any
+from typing import Iterable, Union, Any, Mapping, Tuple, List, Dict
 
-from .chip_data_utils import NetData
-from .misc import IcecraftNetPosition, IcecraftLUTPosition
+from .chip_data import ConfigAssemblage
+from .chip_data_utils import NetData, ElementInterface
+from .config_item import ConnectionItem, IndexedItem
+from .misc import IcecraftNetPosition, IcecraftLUTPosition, TilePosition, IcecraftBitPosition
 
 @total_ordering
 @dataclass(frozen=True, order=False)
 class VertexDesig:
+	"""Wrapper to make IcecraftNetPosition and IcecraftLUTPosition comparable"""
 	position: Union[IcecraftNetPosition, IcecraftLUTPosition]
 	
 	def __lt__(self, other: Any) -> bool:
@@ -56,11 +59,161 @@ class VertexDesig:
 class EdgeDesig:
 	src: VertexDesig
 	dst: VertexDesig
+	
+	def __post_init__(self):
+		assert self.src.tile == self.dst.tile, "src and dst are not in the same tile"
 
-@dataclass(frozen=True)
+@dataclass
 class InterElement:
 	rep: "InterRep"
 
+@dataclass
+class SourceGroup:
+	bits: Tuple[IcecraftBitPosition, ...]
+	dst: VertexDesig
+	srcs: Mapping[EdgeDesig, Tuple[bool, ...]]
+	
+	def __post_init__(self) -> None:
+		for b in self.bits:
+			assert b.tile == self.dst.tile
+		for s, v in self.srcs.items():
+			assert s.dst == self.dst
+			assert len(v) == len(self.bits)
+
+@dataclass
+class EdgeData:
+	available: bool = field(default=True, init=False)
+
+@dataclass
+class Vertex(InterElement):
+	available: bool = field(default=True, init=False)
+	in_data: Dict[EdgeDesig, EdgeData] = field(default_factory=dict, init=False)
+	out_edges: List[EdgeDesig] = field(default_factory=list, init=False)
+	ext_src: bool = field(default=False, init=False)
+	
+	def add_edge(self, edge: EdgeDesig, incoming: bool) -> None:
+		if incoming:
+			self.in_data[edge] = EdgeData()
+		else:
+			self.out_edges.append(edge)
+	
+	def get_in_data(self, edge: EdgeDesig) -> EdgeData:
+		return self.in_data[edge]
+	
+	def iter_in_edges(self) -> Iterable[EdgeDesig]:
+		yield from self.in_data.keys()
+	
+	def iter_out_edges(self) -> Iterable[EdgeDesig]:
+		yield from self.out_edges
+
+@dataclass
+class ConVertex(Vertex):
+	desigs: Tuple[VertexDesig, ...]
+	hard_driven: bool
+	drivers: Tuple[int, ...]
+	src_grps: List[SourceGroup] = field(default_factory=list, init=False)
+	
+	def add_src_grp(self, con_item: ConnectionItem) -> None:
+		dst = VertexDesig(IcecraftNetPosition(con_item.bits[0].tile, con_item.dst_net))
+		assert dst in self.desigs, "Wrong dst for ConnectionItem "
+		
+		srcs = {}
+		for value, src_net in zip(con_item.values, con_item.src_nets):
+			src = VertexDesig(IcecraftNetPosition(dst.tile, src_net))
+			edge = EdgeDesig(src, dst)
+			srcs[edge] = value
+			self.rep.add_edge(edge)
+		
+		src_grp = SourceGroup(con_item.bits, dst, srcs)
+		self.src_grps.append(src_grp)
+	
+	@classmethod
+	def from_net_data(cls, rep: "InterRep", raw: NetData) -> "ConVertex":
+		desigs = tuple(VertexDesig(IcecraftNetPosition.from_coords(*s)) for s in raw.segment)
+		return cls(rep, desigs, raw.hard_driven, raw.drivers)
+
+@dataclass
+class LUTVertex(Vertex):
+	desig: VertexDesig
+	truth_table_bits: Tuple[IcecraftBitPosition, ...]
+	inputs: List[EdgeDesig] = field(default_factory=list) # an ordered representation of the LUT inputs is required (in edges are unordered)
+	
+	def __post_init__(self):
+		tile = self.desig.tile
+		for b in self.truth_table_bits:
+			assert b.tile == tile
+	
+	def connect(self, lut_con: ElementInterface) -> None:
+		for in_net in lut_con.in_nets:
+			src = VertexDesig(IcecraftNetPosition.from_coords(*in_net))
+			in_edge = EdgeDesig(src, self.desig)
+			self.rep.add_edge(in_edge)
+			self.inputs.append(in_edge)
+		
+		for out_net in lut_con.out_nets:
+			dst = VertexDesig(IcecraftNetPosition.from_coords(*out_net))
+			out_edge = EdgeDesig(self.desig, dst)
+			self.rep.add_edge(out_edge)
+	
+	@property
+	def desigs(self):
+		return (self.desig, )
+	
+	@classmethod
+	def from_truth_table(cls, rep: "InterRep", tt_config: IndexedItem) -> "LUTVertex":
+		if tt_config.kind != "TruthTable":
+			raise ValueError(f"Need config kind 'TruthTable', got {tt_config.kind}")
+		tile = tt_config.bits[0].tile
+		desig = VertexDesig(IcecraftLUTPosition(tile, tt_config.index))
+		return cls(rep, desig, tt_config.bits)
+
 class InterRep:
-	def __init__(self, net_data_iter: Iterable[NetData]) -> None:
-		pass
+	def __init__(self, net_data_iter: Iterable[NetData], config_map: Mapping[TilePosition, ConfigAssemblage]) -> None:
+		self._vertices = []
+		self._vertex_map = {}
+		self._edges = []
+		
+		for raw_net in net_data_iter:
+			self._add_con_vertex(raw_net)
+		
+		for tile, config_assem in config_map.items():
+			# add LUT vertices
+			for lut_grp in config_assem.lut:
+				for lut_config in lut_grp:
+					if lut_config.kind != "TruthTable":
+						continue
+					
+					self._add_lut_vertex(lut_config)
+		# TODO: CARRY_ONE_IN, RAM and D_IN
+		# add configs
+	
+	def _add_vertex(self, vertex: Vertex) -> None:
+		self._vertices.append(vertex)
+		for des in vertex.desigs:
+			self._vertex_map[des] = vertex
+	
+	def _add_con_vertex(self, raw_net: NetData) -> None:
+		vertex = ConVertex.from_net_data(self, raw_net)
+		self._add_vertex(vertex)
+	
+	def _add_lut_vertex(self, tt_config: IndexedItem) -> None:
+		vertex = LUTVertex.from_truth_table(self, tt_config)
+		self._add_vertex(vertex)
+	
+	def add_edge(self, edge: EdgeDesig) -> None:
+		src = self.get_vertex(edge.src)
+		src.add_edge(edge, False)
+		
+		dst = self.get_vertex(edge.dst)
+		dst.add_edge(edge, True)
+		
+		self._edges.append(edge)
+	
+	def get_vertex(self, desig: VertexDesig) -> Vertex:
+		return self._vertex_map[desig]
+	
+	def iter_vertices(self) -> Iterable[Vertex]:
+		yield from self._vertices
+	
+	def iter_edges(self) -> Iterable[EdgeDesig]:
+		yield from self._edges
--- tests/icecraft/test_inter_rep.py
+++ tests/icecraft/test_inter_rep.py
@@ -1,27 +1,32 @@
 import unittest
 import operator
+import copy
+
+from typing import NamedTuple, Iterable, Mapping
 
 from adapters.icecraft import TilePosition, IcecraftBitPosition, IcecraftNetPosition, IcecraftLUTPosition
-from adapters.icecraft.inter_rep import InterRep, VertexDesig, EdgeDesig
-from adapters.icecraft.chip_data_utils import NetData
-from adapters.icecraft.config_item import ConnectionItem
+from adapters.icecraft.inter_rep import InterRep, VertexDesig, EdgeDesig, EdgeData, SourceGroup, Vertex, ConVertex, LUTVertex
+from adapters.icecraft.chip_data import ConfigAssemblage
+from adapters.icecraft.chip_data_utils import NetData, ElementInterface
+from adapters.icecraft.config_item import ConnectionItem, IndexedItem
 
 from .common import create_bits
 
 NET_DATA = (
 	NetData(((2, 3, "internal"), ), False, (0,)), # 0
 	NetData(((2, 3, "internal_2"), ), False, (0,)), # 1
-	NetData(((0, 3, "right"), (1, 3, "out"), (2, 3, "left")), True, (1, )), # 2
-	NetData(((0, 3, "wire_in_1"), (1, 3, "wire_in_2"), (2, 3, "wire_out")), False, (0, 1)), # 3
-	NetData(((2, 3, "empty_out"), ), False, tuple()), # 4 no driver
-	NetData(((4, 2, "short_span_1"), (4, 3, "short_span_1")), False, (0, 1)), # 5
-	NetData(((4, 1, "short_span_2"), (4, 2, "short_span_2")), False, (0, 1)), # 6
-	NetData(((4, 2, "out"), ), True, (0, )), # 7
-	NetData(((5, 0, "long_span_1"), (5, 3, "long_span_1")), False, (0, 1)), # 8
-	NetData(((5, 3, "long_span_2"), (8, 3, "long_span_2")), False, (0, 1)), # 9
-	NetData(((8, 0, "long_span_3"), (8, 3, "long_span_3")), False, (0, 1)), # 10
-	NetData(((5, 0, "long_span_4"), (7, 0, "long_span_4"), (8, 0, "long_span_4")), False, (0, 1, 2)), # 11
-	NetData(((7, 0, "out"), ), True, (0, )), # 12
+	NetData(((2, 3, "lut_out"), ), True, (0, )), # 2
+	NetData(((0, 3, "right"), (1, 3, "out"), (2, 3, "left")), True, (1, )), # 3
+	NetData(((0, 3, "wire_in_1"), (1, 3, "wire_in_2"), (2, 3, "wire_out")), False, (0, 1)), # 4
+	NetData(((2, 3, "empty_out"), ), False, tuple()), # 5 no driver
+	NetData(((4, 2, "short_span_1"), (4, 3, "short_span_1")), False, (0, 1)), # 6
+	NetData(((4, 1, "short_span_2"), (4, 2, "short_span_2")), False, (0, 1)), # 7
+	NetData(((4, 2, "out"), ), True, (0, )), # 8
+	NetData(((5, 0, "long_span_1"), (5, 3, "long_span_1")), False, (0, 1)), # 9
+	NetData(((5, 3, "long_span_2"), (8, 3, "long_span_2")), False, (0, 1)), # 10
+	NetData(((8, 0, "long_span_3"), (8, 3, "long_span_3")), False, (0, 1)), # 11
+	NetData(((5, 0, "long_span_4"), (7, 0, "long_span_4"), (8, 0, "long_span_4")), False, (0, 1, 2)), # 12
+	NetData(((7, 0, "out"), ), True, (0, )), # 13
 )
 
 # left, wire_out -> internal
@@ -70,8 +75,24 @@ CON_DATA = (
 	), # 8
 	ConnectionItem(
 		(IcecraftBitPosition.from_coords(7, 0, 5, 3), ),
-		"connection", "long_span_4", ((True), ), ("out", )
+		"connection", "long_span_4", ((True, ), ), ("out", )
 	), # 9
+)
+
+LUT_DATA = tuple(
+	tuple(IndexedItem(create_bits(2, 3, b), k, 3) for b, k in r)
+	for r in ((
+		(((14, 44),), 'CarryEnable'), (((14, 45),), 'DffEnable'),
+		(((15, 44),), 'Set_NoReset'), (((15, 45),), 'AsyncSetReset'),
+		(((14, 40), (15, 40), (15, 41), (14, 41)), 'TruthTable')
+	), )
+)
+
+LUT_CON = (
+	ElementInterface(
+		((2, 3, "internal"), (2, 3, "internal_2")),
+		((2, 3, "lut_out"), )
+	),
 )
 
 class TestDesignation(unittest.TestCase):
@@ -96,6 +117,9 @@ class TestDesignation(unittest.TestCase):
 		
 		return edge_desigs
 	
+	def test_creation_vertex_desig(self):
+		self.create_vertex_desigs()
+	
 	def test_creation_edge_desig(self):
 		vert_desigs = self.create_vertex_desigs()
 		for vd_1 in vert_desigs[:2]:
@@ -112,28 +136,390 @@ class TestDesignation(unittest.TestCase):
 				for i_2, ed_2 in enumerate(edge_desigs):
 					with self.subTest(op=op, ed_1=ed_1, ed_2=ed_2):
 						self.assertEqual(op(order[i_1], order[i_2]), op(ed_1, ed_2))
+	
+	def test_edge_post_init_check(self):
+		src = VertexDesig(IcecraftNetPosition.from_coords(4, 1, "net_a"))
+		dst = VertexDesig(IcecraftNetPosition.from_coords(5, 1, "net_b"))
+		
+		with self.assertRaises(AssertionError):
+			EdgeDesig(src, dst)
+
+class TestSourceGroup(unittest.TestCase):
+	def test_creation(self):
+		tile = TilePosition(2, 34)
+		bits = (IcecraftBitPosition(tile, 4, 5), )
+		dst = VertexDesig(IcecraftNetPosition(tile, "net_a"))
+		src = VertexDesig(IcecraftNetPosition(tile, "net_b"))
+		edge = EdgeDesig(src, dst)
+		dut = SourceGroup(bits, dst, {edge: (True, )})
+	
+	def test_post_init_checks(self):
+		tile = TilePosition(2, 34)
+		other_tile = TilePosition(3, 45)
+		bits = (IcecraftBitPosition(tile, 4, 5), IcecraftBitPosition(tile, 6, 5))
+		dst = VertexDesig(IcecraftNetPosition(tile, "net_a"))
+		src_1 = VertexDesig(IcecraftNetPosition(tile, "net_b"))
+		src_2 = VertexDesig(IcecraftNetPosition(tile, "net_c"))
+		edge_1 = EdgeDesig(src_1, dst)
+		edge_2 = EdgeDesig(src_1, dst)
+		edge_map = {edge_1: (False, True), edge_2: (True, True)}
+		
+		with self.subTest(desc="wrong value length"):
+			broken_edge_map = {edge_1: (False, True, False), edge_2: (True, True, False)}
+			with self.assertRaises(AssertionError):
+				dut = SourceGroup(bits, dst, broken_edge_map)
+		
+		with self.subTest(desc="wrong tile in edge src and dst"):
+			other_dst = VertexDesig(IcecraftNetPosition(other_tile, "net_a"))
+			other_src_1 = VertexDesig(IcecraftNetPosition(other_tile, "net_b"))
+			other_src_2 = VertexDesig(IcecraftNetPosition(other_tile, "net_c"))
+			other_edge_1 = EdgeDesig(other_src_1, other_dst)
+			other_edge_2 = EdgeDesig(other_src_1, other_dst)
+			
+			with self.assertRaises(AssertionError):
+				dut = SourceGroup(bits, dst, {other_edge_1: (False, True), other_edge_2: (True, True)})
+		
+		with self.subTest(desc="wrong tile in bits"):
+			other_bits = (IcecraftBitPosition(other_tile, 4, 5), IcecraftBitPosition(other_tile, 6, 5))
+			
+			with self.assertRaises(AssertionError):
+				dut = SourceGroup(other_bits, dst, edge_map)
 
 class TestInterRep(unittest.TestCase):
-	def test_creation(self):
-		with self.subTest(desc="no input"):
-			dut = InterRep([])
+	def create_config_map(self):
+		config_map = {}
 		
-		with self.subTest(desc="initial net data"):
-			dut = InterRep(NET_DATA)
-	# vertex from name
-	# edge from name?
-	# endpoints of edge
-	# add Connectionconfig, update ConVertex
-	# add LUT truth table, create LUTVertex
+		tile = LUT_DATA[0][0].bits[0].tile
+		config_map.setdefault(tile, ConfigAssemblage()).lut = LUT_DATA
+		
+		return config_map
 	
+	def test_creation(self):
+		class InterRepCreation(NamedTuple):
+			desc: str
+			net_data: Iterable[NetData]
+			config_map: Mapping[TilePosition, ConfigAssemblage]
+		
+		test_cases = (
+			InterRepCreation("no input", [], {}),
+			InterRepCreation("initial net data", NET_DATA, {}),
+			InterRepCreation("initial net data and LUT config", NET_DATA, self.create_config_map())
+		)
+		
+		for tc in test_cases:
+			with self.subTest(desc=tc.desc):
+				dut = InterRep(tc.net_data, tc.config_map)
+				self.check_consistency(self, dut)
+				self.check_initial_data(tc.net_data, tc.config_map, dut)
+	
+	def check_initial_data(self, net_data_iter, config_map, rep):
+		for raw_net in net_data_iter:
+			desig = VertexDesig(IcecraftNetPosition.from_coords(*raw_net.segment[0]))
+			self.assertIn(desig, rep._vertex_map)
+			vertex = rep.get_vertex(desig)
+			self.check_con_vertex(rep, raw_net, desig, vertex)
+		
+		for tile, config_assem in config_map.items():
+			for lut_grp in config_assem.lut:
+				tt_list = [c for c in lut_grp if c.kind == "TruthTable"]
+				tt_item = tt_list[0]
+				desig =  VertexDesig(IcecraftLUTPosition(tile, tt_item.index))
+				self.assertIn(desig, rep._vertex_map)
+				vertex = rep.get_vertex(desig)
+				
+				self.check_lut_vertex(rep, tt_item, vertex)
+			#TODO: fixed connections
+	
+	def check_con_vertex(self, rep, raw_net, desig, vertex):
+		self.assertIn(desig, vertex.desigs)
+		self.assertEqual(set(raw_net.segment), set((*d.tile, d.position.name) for d in vertex.desigs))
+		self.assertEqual(raw_net.hard_driven, vertex.hard_driven)
+		self.assertEqual(raw_net.drivers, vertex.drivers)
+		self.assertEqual(rep, vertex.rep)
+	
+	def test_get_vertex(self):
+		dut = InterRep(NET_DATA, {})
+		
+		for raw_net in NET_DATA:
+			with self.subTest(raw_net=raw_net):
+				for seg in raw_net.segment:
+					desig = VertexDesig(IcecraftNetPosition.from_coords(*seg))
+					res = dut.get_vertex(desig)
+					
+					self.check_con_vertex(dut, raw_net, desig, res)
+					self.check_consistency(self, dut)
+	
+	def test_add_con_vertex(self):
+		dut = InterRep(NET_DATA, {})
+		existing_vertices = []
+		
+		for raw_net in NET_DATA:
+			with self.subTest(raw_net=raw_net):
+				dut._add_con_vertex(raw_net)
+				
+				desig = VertexDesig(IcecraftNetPosition.from_coords(*raw_net.segment[0]))
+				res = dut.get_vertex(desig)
+				
+				self.check_con_vertex(dut, raw_net, desig, res)
+				self.check_consistency(self, dut)
+				
+				existing_vertices.append(res)
+				
+				for exp_vtx in existing_vertices:
+					res = dut.get_vertex(exp_vtx.desigs[0])
+					self.assertEqual(exp_vtx, res)
+		
+		# add Connectionconfig, update ConVertex
+	
+	def check_lut_vertex(self, rep, tt_item, vertex):
+		self.assertEqual(tt_item.bits, vertex.truth_table_bits)
+		self.assertEqual(tt_item.index, vertex.desig.position.z)
+		self.assertEqual(tt_item.bits[0].tile, vertex.desig.tile)
+		self.assertEqual(rep, vertex.rep)
+	
+	def test_add_lut_vertex(self):
+		dut = InterRep(NET_DATA, {})
+		existing_vertices = []
+		
+		for lut_items in LUT_DATA:
+			tt_item = lut_items[-1]
+			with self.subTest(tt_item=tt_item):
+				dut._add_lut_vertex(tt_item)
+				
+				desig = VertexDesig(IcecraftLUTPosition(tt_item.bits[0].tile, tt_item.index))
+				res = dut.get_vertex(desig)
+				
+				self.check_lut_vertex(dut, tt_item, res)
+				self.check_consistency(self, dut)
+	
+	# add LUT truth table, create LUTVertex
+	@staticmethod
+	def check_consistency(test_case, rep):
+		for edge in rep.iter_edges():
+			# all src/dst in vertices
+			test_case.assertIn(edge.src, rep._vertex_map)
+			test_case.assertIn(edge.dst, rep._vertex_map)
+			
+			# all edges in vertex in/out
+			src_vtx = rep.get_vertex(edge.src)
+			test_case.assertIn(edge, src_vtx.out_edges)
+			dst_vtx = rep.get_vertex(edge.dst)
+			test_case.assertIn(edge, dst_vtx.in_data)
+		
+		edge_set = set(rep.iter_edges())
+		# all vertex in out in edges
+		for vertex in rep.iter_vertices():
+			for edge in vertex.in_data:
+				test_case.assertIn(edge.dst, vertex.desigs)
+				test_case.assertIn(edge, edge_set)
+			
+			for edge in vertex.out_edges:
+				test_case.assertIn(edge.src, vertex.desigs)
+				test_case.assertIn(edge, edge_set)
 
 class TestVertex(unittest.TestCase):
-	# get incoming edges
-	# get outgoing edges
-	pass
+	def create_vertex(self):
+		rep = InterRep([], {})
+		return Vertex(rep)
+	
+	def test_creation(self):
+		dut = self.create_vertex()
+	
+	def test_in_edges(self):
+		dut = self.create_vertex()
+		tile = TilePosition(7, 2)
+		exp = []
+		
+		with self.subTest(desc="empty"):
+			res = list(dut.iter_in_edges())
+			self.assertEqual(exp, res)
+		
+		dst = VertexDesig(IcecraftNetPosition(tile, "net_a"))
+		src = VertexDesig(IcecraftNetPosition(tile, "net_b"))
+		edge = EdgeDesig(src, dst)
+		exp.append(edge)
+		
+		with self.subTest(desc="add edge to empty vertex"):
+			dut.add_edge(edge, True)
+			res = list(dut.iter_in_edges())
+			self.assertEqual(exp, res)
+			
+			res_data = dut.get_in_data(edge)
+			self.assertEqual(EdgeData(), res_data)
+	
+	def test_out_edges(self):
+		dut = self.create_vertex()
+		tile = TilePosition(7, 2)
+		exp = []
+		
+		with self.subTest(desc="empty"):
+			res = list(dut.iter_out_edges())
+			self.assertEqual(exp, res)
+		
+		dst = VertexDesig(IcecraftNetPosition(tile, "net_a"))
+		src = VertexDesig(IcecraftNetPosition(tile, "net_b"))
+		edge = EdgeDesig(src, dst)
+		exp.append(edge)
+		
+		with self.subTest(desc="add edge to empty vertex"):
+			dut.add_edge(edge, False)
+			res = list(dut.iter_out_edges())
+			self.assertEqual(exp, res)
+		
 
 class TestConVertex(unittest.TestCase):
+	def test_creation(self):
+		desig_1 = VertexDesig(IcecraftNetPosition.from_coords(5, 1, "net_a"))
+		desig_2 = VertexDesig(IcecraftNetPosition.from_coords(6, 25, "net_b"))
+		rep = InterRep([], {})
+		
+		with self.subTest(desc="hard driven"):
+			dut = ConVertex(rep, (desig_1, ), True, (0, ))
+		
+		with self.subTest(desc="multi desig"):
+			dut = ConVertex(rep, (desig_1, desig_2), False, (1, ))
+	
+	def test_from_net_data(self):
+		for raw_net in NET_DATA:
+			with self.subTest(raw_net=raw_net):
+				rep = InterRep([], {})
+				dut = ConVertex.from_net_data(rep, raw_net)
+				
+				self.assertEqual(rep, dut.rep)
+				self.assertEqual(raw_net.hard_driven, dut.hard_driven)
+				self.assertEqual(raw_net.drivers, dut.drivers)
+				self.assertEqual(set(raw_net.segment), set((*d.tile, d.position.name) for d in dut.desigs))
+	
+	def test_add_src_grp(self):
+		rep = InterRep(NET_DATA, {})
+		TestInterRep.check_consistency(self, rep)
+		const_attrs = ("available", "ext_src", "out_edges", "desigs", "hard_driven", "drivers")
+		
+		for con_item in CON_DATA:
+			with self.subTest(con_item=con_item):
+				desig = VertexDesig(IcecraftNetPosition(con_item.bits[0].tile, con_item.dst_net))
+				
+				dut = rep.get_vertex(desig)
+				prev_vals = {a: copy.copy(getattr(dut, a)) for a in const_attrs}
+				prev_in_data = copy.copy(dut.in_data)
+				prev_src_grps = copy.copy(dut.src_grps)
+				
+				dut.add_src_grp(con_item)
+				
+				# check values that stay the same
+				self.assertEqual(rep, dut.rep)
+				for attr in const_attrs:
+					self.assertEqual(prev_vals[attr], getattr(dut, attr))
+				for src_grp in prev_src_grps:
+					self.assertIn(src_grp, dut.src_grps)
+				
+				# check new source group
+				new_grps = [s for s in dut.src_grps if s not in prev_src_grps]
+				self.assertEqual(1, len(new_grps))
+				src_grp = new_grps[0]
+				self.assertEqual(con_item.bits, src_grp.bits)
+				self.assertIn(src_grp.dst, dut.desigs)
+				value_map = {n: v for n, v in zip(con_item.src_nets, con_item.values)}
+				for edge, values in src_grp.srcs.items():
+					exp_vals = value_map[edge.src.position.name]
+					self.assertEqual(exp_vals, values)
+					self.assertIn(edge.dst, dut.desigs)
+					
+				
+				TestInterRep.check_consistency(self, rep)
+	
 	# handle externally driven
 	# multiple driver tiles
 	# get bits and list of possibilities
-	pass
+
+class TestLUTVertex(unittest.TestCase):
+	def test_creation(self):
+		tile = TilePosition(4, 21)
+		index = 4
+		rep = InterRep([], {})
+		bits = (IcecraftBitPosition(tile, 4, 5), IcecraftBitPosition(tile, 6, 5))
+		desig = VertexDesig(IcecraftLUTPosition(tile, index))
+		
+		dut = LUTVertex(rep, desig, bits)
+		
+		self.assertEqual(bits, dut.truth_table_bits)
+		self.assertEqual(index, dut.desig.position.z)
+		self.assertEqual(desig, dut.desig)
+		self.assertEqual(rep, dut.rep)
+	
+	def test_from_truth_table(self):
+		for lut_items in LUT_DATA:
+			tt_item = lut_items[-1]
+			rep = InterRep([], {})
+			with self.subTest(tt_item=tt_item):
+				dut = LUTVertex.from_truth_table(rep, tt_item)
+				
+				self.assertEqual(tt_item.bits, dut.truth_table_bits)
+				self.assertEqual(tt_item.index, dut.desig.position.z)
+				self.assertEqual(tt_item.bits[0].tile, dut.desig.tile)
+				self.assertEqual(rep, dut.rep)
+	
+	def test_desigs(self):
+		for lut_items in LUT_DATA:
+			tt_item = lut_items[-1]
+			rep = InterRep([], {})
+			with self.subTest(tt_item=tt_item):
+				dut = LUTVertex.from_truth_table(rep, tt_item)
+				
+				res = dut.desigs
+				self.assertIn(dut.desig, res)
+				self.assertEqual(1, len(res))
+	
+	def test_post_init_checks(self):
+		tile = TilePosition(4, 21)
+		other_tile = TilePosition(5, 21)
+		rep = InterRep([], {})
+		bits = (IcecraftBitPosition(tile, 4, 5), IcecraftBitPosition(tile, 6, 5))
+		desig = VertexDesig(IcecraftLUTPosition(tile, 5))
+		
+		with self.subTest(desc="desig and bits inconsistent"):
+			other_desig = VertexDesig(IcecraftLUTPosition(other_tile, 5))
+			
+			with self.assertRaises(AssertionError):
+				dut = LUTVertex(rep, other_desig, bits)
+		
+		with self.subTest(desc="inconsistent bits"):
+			other_bits = (IcecraftBitPosition(tile, 4, 5), IcecraftBitPosition(other_tile, 6, 5))
+			
+			with self.assertRaises(AssertionError):
+				dut = LUTVertex(rep, desig, other_bits)
+	
+	def test_connect(self):
+		for lut_items, lut_con in zip(LUT_DATA, LUT_CON):
+			tt_item = lut_items[-1]
+			rep = InterRep(NET_DATA, {})
+			with self.subTest(tt_item=tt_item):
+				dut = LUTVertex.from_truth_table(rep, tt_item)
+				rep._add_vertex(dut)
+				
+				# unconnected before call
+				self.assertEqual(0, len(dut.in_data))
+				self.assertEqual(0, len(dut.out_edges))
+				
+				dut.connect(lut_con)
+				
+				# connected after call
+				self.assertEqual(set(dut.in_data.keys()), set(dut.inputs))
+				self.assertEqual(len(lut_con.in_nets), len(dut.inputs))
+				# order has to be preserved for inputs
+				for in_desig, in_net in zip(dut.inputs, lut_con.in_nets):
+					self.assertEqual(in_net[0], in_desig.src.tile.x)
+					self.assertEqual(in_net[1], in_desig.src.tile.y)
+					self.assertEqual(in_net[2], in_desig.src.position.name)
+					self.assertEqual(dut.desig, in_desig.dst)
+				
+				self.assertEqual(len(lut_con.out_nets), len(dut.out_edges))
+				# order preservation is not expected for out_edges
+				out_net_set = set(lut_con.out_nets)
+				for out_edge in dut.out_edges:
+					self.assertEqual(dut.desig, out_edge.src)
+					dst = out_edge.dst
+					self.assertIn((*dst.tile, dst.position.name), out_net_set)
+				
+				TestInterRep.check_consistency(self, rep)
+		
