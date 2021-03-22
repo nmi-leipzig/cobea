4a5e8fe3629192951e234f3a3986655e9921ad13
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2021-03-22 18:37:49 +0100

    [ADD] icecraft position transformation library

---
+++ adapters/icecraft/position_transformation.py
@@ -0,0 +1,28 @@
+from functools import partial
+from typing import Sequence, List
+
+from domain.interfaces import PosTransLibrary
+from domain.model import PosTransImpl
+from domain.request_model import RequestObject, ParameterValues
+from adapters.icecraft.misc import TilePosition
+
+class IcecraftPosTransLibrary(PosTransLibrary):
+	def __init__(self) -> None:
+		self.param_dict = {
+			"expand_rectangle": tuple()
+		}
+	
+	def get_implementation(self, request: RequestObject) -> PosTransImpl:
+		func = getattr(self, request.identifier)
+		params = ParameterValues()
+		for name in self.param_dict[request.identifier]:
+			params[name] = request[name]
+		return partial(func, params=params)
+	
+	@staticmethod
+	def expand_rectangle(corner_points: Sequence[TilePosition], params=None) -> List[TilePosition]:
+		x_min = min(t.x for t in corner_points)
+		x_max = max(t.x for t in corner_points)
+		y_min = min(t.y for t in corner_points)
+		y_max = max(t.y for t in corner_points)
+		return [TilePosition(x, y) for x in range(x_min, x_max+1) for y in range(y_min, y_max+1)]
---
+++ tests/icecraft/test_position_transformation.py
@@ -0,0 +1,62 @@
+import unittest
+
+from domain.request_model import RequestObject
+from domain.use_cases import CreatePosTrans
+from adapters.icecraft.position_transformation import IcecraftPosTransLibrary
+from adapters.icecraft.misc import TilePosition
+
+class TestIcecraftPosTransLibrary(unittest.TestCase):
+	def test_creation(self):
+		ptl = IcecraftPosTransLibrary()
+	
+	def test_use_case_creation(self):
+		ptl = IcecraftPosTransLibrary()
+		uc = CreatePosTrans(ptl)
+	
+	def test_expand_rectangle(self):
+		dut = IcecraftPosTransLibrary.expand_rectangle
+		
+		self.check_expand_rectangle(dut)
+	
+	def test_expand_rectangle_req(self):
+		ptl = IcecraftPosTransLibrary()
+		
+		req = RequestObject()
+		req["identifier"] = "expand_rectangle"
+		dut = ptl.get_implementation(req)
+		
+		self.check_expand_rectangle(dut)
+	
+	def test_use_case(self):
+		ptl = IcecraftPosTransLibrary()
+		uc = CreatePosTrans(ptl)
+		
+		req = RequestObject()
+		req["identifier"] = "expand_rectangle"
+		req["description"] = "from corners to all tiles in rectangle"
+		
+		dut = uc(req)
+		
+		self.check_expand_rectangle(dut)
+	
+	def check_expand_rectangle(self, dut):
+		test_data = (
+			((2, 2, 2, 2), [(2, 2)]), # single tile
+			((3, 5, 7, 5), [(3, 5), (4, 5), (5, 5), (6, 5), (7, 5)]), # row
+			((7, 9, 7, 13), [(7, 9), (7, 10), (7, 11), (7, 12), (7, 13)]), # colum
+			((4, 6, 5, 7), [(4, 6), (4, 7), (5, 6), (5, 7)]), # no inner tile
+			((5, 8, 7, 10), [(5, 8), (5, 9), (5, 10), (6, 8), (6, 9), (6, 10), (7, 8), (7, 9), (7, 10)]), # inner tile
+		)
+		
+		for rect, raw_exp in test_data:
+			exp = [TilePosition(*t) for t in raw_exp]
+			res = dut([TilePosition(*rect[:2]), TilePosition(*rect[2:4])])
+			res_set = set(res)
+			
+			# no duplicates
+			self.assertEqual(len(res), len(res_set))
+			
+			# correct tiles
+			self.assertEqual(set(exp), res_set)
+	
+		
