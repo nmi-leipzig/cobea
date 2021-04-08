1159ca60bd68083945f3f15a78ca48513956f1a3
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2021-04-08 13:09:21 +0200

    [ADD] use case for generating a chromosome; [ADD] use case for generating a random chromosome

--- domain/use_cases.py
+++ domain/use_cases.py
@@ -2,8 +2,9 @@ from abc import ABC, abstractmethod
 from typing import Any, Dict, Mapping, Iterable
 
 from domain.model import OutputData, Chromosome
-from domain.interfaces import FitnessFunctionLibrary, Preprocessing, PreprocessingLibrary, EvoAlgo, DataSink\
-TargetManager, Meter, RepresentationGenerator, Representation, PosTrans, PosTransLibrary, TargetConfiguration
+from domain.interfaces import FitnessFunctionLibrary, Preprocessing, PreprocessingLibrary, EvoAlgo, DataSink, PRNG,\
+TargetManager, Meter, RepresentationGenerator, Representation, PosTrans, PosTransLibrary, TargetConfiguration,\
+UniqueID
 from domain.request_model import RequestObject, ParameterUser, Parameter
 
 class UseCase(ParameterUser):
@@ -42,8 +43,37 @@ class RunEvoAlgo(UseCase):
 		self._target_manager = target_manager
 		self._meter = meter
 		self._data_sink = data_sink
-		self._parameters = {}
+		self._parameters = {"perform": []}
 	
 	def perform(self, request: RequestObject) -> None:
 		with self._data_sink:
 			self._evo_algo.run()
+
+class GenChromo(UseCase):
+	"""Generate Chromosome"""
+	
+	def __init__(self, uid_gen: UniqueID) -> None:
+		self._uid_gen = uid_gen
+		self._parameters = {
+			"perform": [Parameter("allele_indices", int, multiple=True)]
+		}
+	
+	def perform(self, request: RequestObject) -> Chromosome:
+		new_id = self._uid_gen.get_id()
+		indices = tuple(request.allele_indices)
+		chromo = Chromosome(new_id, indices)
+		return chromo
+
+class RandomChromo(UseCase):
+	"""Generate a random chromosome"""
+	
+	def __init__(self, prng: PRNG, rep: Representation, uid_gen: UniqueID) -> None:
+		self._prng = prng
+		self._rep = rep
+		self._chromo_gen = GenChromo(uid_gen)
+		
+		self._parameters = {"perform": []}
+	
+	def perform(self, request: RequestObject) -> None:
+		indices = [self._prng.randint(0, len(g.alleles)-1) for g in self._rep.iter_genes()]
+		return self._chromo_gen(RequestObject(allele_indices=indices))
--- tests/domain/test_use_cases.py
+++ tests/domain/test_use_cases.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
-import unittest.mock as mock
+import random
 import sys
-
 import unittest
+import unittest.mock as mock
 
-from domain.model import InputData, OutputData
-from domain.use_cases import Measure
+from domain.allele_sequence import Allele, AlleleList, AlleleAll, AllelePow
+from domain.model import InputData, OutputData, Gene
+from domain.use_cases import Measure, GenChromo, RandomChromo
 from domain.request_model import RequestObject
 
-from ..mocks import MockTargetManager, MockMeter
+from ..mocks import MockTargetManager, MockMeter, MockUniqueID, MockRandInt, MockRepresentation, MockBitPos
 from ..common import check_parameter_user
 
 class MeasureTest(unittest.TestCase):
@@ -35,3 +36,68 @@ class MeasureTest(unittest.TestCase):
 		
 		measure_case = Measure(mock_manager, mock_meter)
 		check_parameter_user(self, measure_case)
+
+class GenChromoTest(unittest.TestCase):
+	def setUp(self):
+		self.id_list = [3, 4, 28, 0, 10]
+		self.mock_id =  MockUniqueID(self.id_list)
+	
+	def test_call(self):
+		allele_indices_list = [[j*j for j in range(i+1)] for i in range(len(self.id_list))]
+		
+		dut = GenChromo(self.mock_id)
+		
+		for exp_id, exp_indices in zip(self.id_list, allele_indices_list):
+			res = dut(RequestObject(allele_indices=exp_indices))
+			
+			self.assertEqual(exp_id, res.identifier)
+			self.assertEqual(exp_indices, list(res.allele_indices))
+	
+	def test_parameter_user(self):
+		dut = GenChromo(self.mock_id)
+		
+		check_parameter_user(self, dut)
+
+class RandomChromoTest(unittest.TestCase):
+	def setUp(self):
+		self.id_list = [3, 4, 28, 0, 10]
+		self.mock_id =  MockUniqueID(self.id_list)
+		
+		max_len = 5
+		all_list = []
+		list_list = []
+		pow_list = []
+		for l in range(1, max_len+1):
+			all_list.append(Gene(tuple(MockBitPos(l*100+j) for j in range(l)), AlleleAll(l), ""))
+			list_list.append(Gene(
+				tuple(MockBitPos(l*1000+j) for j in range(l)),
+				AlleleList([Allele((False, )*l, ""), Allele((True, )*l, "")]),
+				""
+			))
+			pow_list.append(Gene(tuple(MockBitPos(l*10000+j) for j in range(l)), AllelePow(l, []), ""))
+		
+		gene_list = all_list + list_list + pow_list
+		self.mock_rep = MockRepresentation(gene_list)
+		
+		int_list = [0]*len(gene_list)
+		int_list.extend([len(g.alleles)-1 for g in gene_list])
+		for _ in range(len(self.id_list)-2):
+			int_list.extend([random.randint(0, len(g.alleles)-1) for g in gene_list])
+		
+		self.int_list = int_list
+		self.mock_prng = MockRandInt(int_list)
+	
+	def test_call(self):
+		gene_count = len(list(self.mock_rep.iter_genes()))
+		dut = RandomChromo(self.mock_prng, self.mock_rep, self.mock_id)
+		
+		for i, exp_id in enumerate(self.id_list):
+			res = dut(RequestObject())
+			
+			self.assertEqual(exp_id, res.identifier)
+			self.assertEqual(self.int_list[i*gene_count:(i+1)*gene_count], list(res.allele_indices))
+	
+	def test_parameter_user(self):
+		dut = RandomChromo(self.mock_prng, self.mock_rep, self.mock_id)
+		
+		check_parameter_user(self, dut)
