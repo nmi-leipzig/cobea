2a01c015124b4f0fe3c4ca9740fb303b33da4868
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2021-05-04 23:34:41 +0200

    [CHANGE] expose IcecraftGeneConstraint, IcecraftInputError, IcecraftResCon, IcecraftResource, TILE_ALL and TILE_ALL_LOGIC in adapters.icecraft

--- adapters/icecraft/__init__.py
+++ adapters/icecraft/__init__.py
@@ -1,6 +1,6 @@
-from .misc import RAMMode, LUTFunction, IcecraftPosition,\
-	IcecraftBitPosition, IcecraftLUTPosition, IcecraftColBufCtrl,\
-	IcecraftNetPosition, IcecraftConnection
+from .misc import IcecraftBitPosition, IcecraftColBufCtrl, IcecraftConnection, IcecraftGeneConstraint,\
+	IcecraftInputError, IcecraftLUTPosition, IcecraftNetPosition, IcecraftPosition, IcecraftResCon,\
+	IcecraftResource, LUTFunction, RAMMode, TILE_ALL, TILE_ALL_LOGIC
 from .configuration import IcecraftRawConfig, IcecraftStormConfig
 from .target import IcecraftDevice, HX8K_BOARD, IcecraftManager, MultiIcecraftManager
 from .meter import IcecraftEmbedMeter
