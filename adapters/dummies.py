9384e2f47b1c5f319bb8fd0fb175a3c2844eaf1f
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2021-06-01 18:21:29 +0200

    [FIX] add missing __enter__ and __exit__ methods to DummyDataSink

--- adapters/dummies.py
+++ adapters/dummies.py
@@ -1,6 +1,7 @@
 """Dummies that do nothing"""
 
-from typing import Any, Mapping
+from types import TracebackType
+from typing import Any, Mapping, Optional, Type
 
 from domain.data_sink import DataSink
 from domain.interfaces import Driver, Meter
@@ -9,6 +10,16 @@ from domain.request_model import RequestObject
 
 class DummyDataSink(DataSink):
 	def write(self, source: str, data_dict: Mapping[str, Any]) -> None:
+		pass
+	
+	def __enter__(self) -> "DummyDataSink":
+		return self
+	
+	def __exit__(self,
+		exc_type: Optional[Type[BaseException]],
+		exc_value: Optional[BaseException],
+		exc_traceback: Optional[TracebackType]
+	) -> bool:
 		pass
 
 class DummyDriver(Driver):
