aa044630413eff119de7aafbb74e53becd6dc77c
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2021-06-04 22:59:34 +0200

    [FIX] missin import in discern_frequency

--- applications/discern_frequency/action.py
+++ applications/discern_frequency/action.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 import time
 
+from contextlib import ExitStack
 from dataclasses import asdict, dataclass
 from datetime import datetime
 from functools import partial
