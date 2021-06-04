54cf3535673a05200cec393d3dd9ce6c6f6790f2
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2021-06-04 12:43:42 +0200

    [ADD] way to access DataSink wrapped in ParallelSink from other processes; [CHANGE] don't hold the mp context in ParallelSink

--- adapters/parallel_sink.py
+++ adapters/parallel_sink.py
@@ -12,19 +12,34 @@ class SinkDetails:
 	args: tuple
 	kwargs: dict
 
+class ParSubSink(DataSink):
+	"""DataSink with access to a ParallelSink, that can be passed to new Processes"""
+	def __init__(self, write_queue: mp.JoinableQueue) -> None:
+		self._write_queue = write_queue
+	
+	def write(self, source: str, data_dict: Mapping[str, Any]) -> None:
+		self._write_queue.put((source, data_dict))
+	
+	def __exit__(self,
+		exc_type: Optional[Type[BaseException]],
+		exc_value: Optional[BaseException],
+		exc_traceback: Optional[TracebackType]
+	) -> bool:
+		return False
+
 class ParallelSink(DataSink):
 	"""Wraps around another DataSink and executes it in a separate process.
 	"""
 	
 	def __init__(self, sink_type: Type[DataSink], sink_args: tuple=tuple(), sink_kwargs: dict={}) -> None:
 		self._sink_details = SinkDetails(sink_type, sink_args, sink_kwargs)
-		self._ctx = mp.get_context("spawn")
 		self._write_queue = None
 		self._process = None
 	
 	def __enter__(self) -> "ParallelSink":
-		self._write_queue = self._ctx.JoinableQueue()
-		self._process = self._ctx.Process(target=self.writer, args=(self._sink_details, self._write_queue))
+		ctx = mp.get_context("spawn")
+		self._write_queue = ctx.JoinableQueue()
+		self._process = ctx.Process(target=self.writer, args=(self._sink_details, self._write_queue))
 		self._process.start()
 		return self
 	
@@ -44,6 +59,15 @@ class ParallelSink(DataSink):
 	
 	def write(self, source: str, data_dict: Mapping[str, Any]) -> None:
 		self._write_queue.put((source, data_dict))
+	
+	def get_sub(self) -> ParSubSink:
+		"""Return a DataSink that writes to the wrapped sink can be passed to other processes
+		
+		As the ParallelSink sink holds a reference to the started Process it can be passed directly to other Process
+		instances. To still use the wrapped DataSink from multiple Process instances, a process safe ParSubSink can
+		be created with this function.
+		"""
+		return ParSubSink(self._write_queue)
 	
 	@staticmethod
 	def writer(sink_details: SinkDetails, write_queue: mp.JoinableQueue) -> None:
