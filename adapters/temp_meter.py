8d4e277ca5fd13be26da12190bfeccf0bf6343bf
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2021-07-19 22:38:44 +0200

    [CHANGE] derive TempMeter from IdentifiableHW; [CHANGE] add property for sensor serial number and type to TempMeter

--- adapters/temp_meter.py
+++ adapters/temp_meter.py
@@ -6,15 +6,17 @@ import time
 from types import TracebackType
 from typing import Iterable, Mapping, Optional, Type
 
-from domain.interfaces import Meter
+from domain.interfaces import IdentifiableHW, Meter
 from domain.model import OutputData
 from domain.request_model import Parameter, RequestObject
 
-class TempMeter(Meter):
+class TempMeter(Meter, IdentifiableHW):
 	"""Read Temperature from Arduino with DS18B20 sensor"""
 	def __init__(self, baudrate=500000)-> None:
 		self._baudrate = baudrate
 		self._arduino = None
+		self._arduino_sn = None
+		self._ds18b20_sn = None
 	
 	@property
 	def parameters(self) -> Mapping[str, Iterable[Parameter]]:
@@ -23,9 +25,26 @@ class TempMeter(Meter):
 			"measure": [],
 		}
 	
+	@property
+	def serial_number(self) -> str:
+		return self._arduino_sn
+	
+	@property
+	def hardware_type(self) -> str:
+		return "Arduino"
+	
+	@property
+	def sensor_serial_number(self) -> str:
+		return self._ds18b20_sn
+	
+	@property
+	def sensor_type(self) -> str:
+		return "DS18B20"
+	
 	def __enter__(self) -> "TempMeter":
 		ports = comports()
 		arduino_ports = [p for p in ports if p.manufacturer and p.manufacturer.startswith("Arduino")]
+		self._arduino_sn = arduino_ports[0].serial_number
 		self._arduino = Serial(port=arduino_ports[0].device, baudrate=self._baudrate)
 		self._arduino.__enter__()
 		self._arduino.reset_input_buffer()
@@ -33,6 +52,11 @@ class TempMeter(Meter):
 		
 		# Arduino reboots on connecting serial -> wait till reboot is done
 		time.sleep(2)
+		
+		self._arduino.write(b"i")
+		sn_data = self._arduino.read(8)
+		sn_int = struct.unpack("<Q", sn)[0]
+		self._ds18b20_sn = f"{sn_int:016x}"
 		
 		return self
 	
