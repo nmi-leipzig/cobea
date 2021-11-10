28637654c64060284fc6123b101774c12a242169
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2021-11-10 18:39:59 +0100

    [CHANGE] test combination of SimtarDev and FixedEmbedDriver

--- tests/simtar/test_target.py
+++ tests/simtar/test_target.py
@@ -1,10 +1,22 @@
 from unittest import TestCase
 
+from adapters.embed_driver import FixedEmbedDriver
+from adapters.embed_meter import EmbedMeter
 from adapters.simtar.config import SimtarConfig
 from adapters.simtar.pos import SimtarBitPos
 from adapters.simtar.target import SimtarDev
+from domain.model import InputData
+from domain.request_model import RequestObject
 
 class SimtarDevTest(TestCase):
+	def setUp(self):
+		self.test_data = [ # config data, expected values
+			([True]*17, [b"\x01"]*16),
+			([False]*17, [b"\xff"]*16),
+			([False]*16+[True], [b"\x00"]*16),
+			([True]*6+[False]*10+[True], [b"\x01"]*6+[b"\x00"]*10)
+		]
+	
 	def test_create(self):
 		dut = SimtarDev()
 	
@@ -20,31 +32,19 @@ class SimtarDevTest(TestCase):
 		dut.configure(config)
 	
 	def test_read_bytes(self):
-		test_data = [ # config data, expected value
-			([True]*17, b"\x01"),
-			([False]*17, b"\xff"),
-			([False]*16+[True], b"\x00"),
-		]
-		
 		dut = SimtarDev()
-		for data, exp in test_data:
+		for data, exp_list in self.test_data :
 			with self.subTest(data=data):
 				config = SimtarConfig(data)
 				dut.configure(config)
 				res = dut.read_bytes(1)
 				
-				self.assertEqual(exp, res)
+				# 0 is default input value
+				self.assertEqual(exp_list[0], res)
 	
 	def test_write_bytes(self):
-		test_data = [ # config data, expected values
-			([True]*17, [b"\x01"]*16),
-			([False]*17, [b"\xff"]*16),
-			([False]*16+[True], [b"\x00"]*16),
-			([True]*6+[False]*10+[True], [b"\x01"]*6+[b"\x00"]*10)
-		]
-		
 		dut = SimtarDev()
-		for data, exp_list in test_data:
+		for data, exp_list in self.test_data:
 			with self.subTest(data=data):
 				config = SimtarConfig(data)
 				dut.configure(config)
@@ -54,3 +54,18 @@ class SimtarDevTest(TestCase):
 					
 					val = dut.read_bytes(1)
 					self.assertEqual(exp, val)
+	
+	def test_driver(self):
+		dut = SimtarDev()
+		drv = FixedEmbedDriver(dut, "B")
+		for data, exp_list in self.test_data:
+			with self.subTest(data=data):
+				config = SimtarConfig(data)
+				dut.configure(config)
+				for i, exp in enumerate(exp_list):
+					req = RequestObject(driver_data=InputData([i]))
+					drv.drive(req)
+					
+					val = dut.read_bytes(1)
+					self.assertEqual(exp, val)
+		
