7d50d514829d5105a244d9cee2acfb79a1b60409
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2022-01-21 16:14:46 +0100

    [CHANGE] check overriding alter in test_pa_gen

--- tests/discern_frequency/test_hdf5_desc.py
+++ tests/discern_frequency/test_hdf5_desc.py
@@ -1,14 +1,23 @@
-from dataclasses import dataclass
-from operator import itemgetter
+from dataclasses import dataclass, fields
+from operator import attrgetter, itemgetter
 from typing import Any, Dict, List
 from unittest import TestCase
 
 
-from adapters.hdf5_sink import MetaEntry, ParamAim
+from adapters.hdf5_sink import chain_funcs, MetaEntry, ParamAim
 from applications.discern_frequency.hdf5_desc import add_meta, HDF5Desc, pa_gen
 
+from ..common import check_func_eq
 
 class HDF5DescTest(TestCase):
+	def check_param_aim(self, a, b):
+		check_func_eq(self, a.alter, b.alter)
+		for cur_field in fields(ParamAim):
+			if cur_field.name == "alter":
+				continue
+			
+			self.assertEqual(getattr(a, cur_field.name), getattr(b, cur_field.name))
+	
 	def test_pa_gen(self):
 		@dataclass
 		class PATC:
@@ -18,13 +27,11 @@ class HDF5DescTest(TestCase):
 			kwargs: Dict[str, Any]
 			exp: ParamAim
 		
-		func = itemgetter(1)
-		
 		cases = [
 			PATC("no kwargs", "habitat", ["in"], {}, ParamAim(["in"], "uint8", "habitat", "/", False, tuple())),
 			PATC("all kwargs", "habitat", ["over", "out"], {
-				"alter": func, "compress": "lzf", "comp_opt": 3, "shuffle": True
-			}, ParamAim(["over", "out"], "uint8", "habitat", "/", False, tuple(), func, "lzf", 3, True)),
+				"alter": itemgetter(1), "compress": "lzf", "comp_opt": 3, "shuffle": True
+			}, ParamAim(["over", "out"], "uint8", "habitat", "/", False, tuple(), itemgetter(1), "lzf", 3, True)),
 			PATC("override type", "habitat", ["in"], {"data_type": "uint16"}, ParamAim(
 				["in"], "uint16", "habitat", "/", False, tuple()
 			)),
@@ -34,12 +41,17 @@ class HDF5DescTest(TestCase):
 			PATC("override type and shape", "habitat", ["in"], {"data_type": "uint16", "shape": (6, )}, ParamAim(
 				["in"], "uint16", "habitat", "/", False, (6, )
 			)),
+			PATC("override alter", "habitat", ["in"], {"alter": chain_funcs([itemgetter(8), attrgetter("abc")])},
+				ParamAim(["in"], "uint8", "habitat", "/", False, tuple(), 
+					alter=chain_funcs([itemgetter(8), attrgetter("abc")])
+				)
+			),
 		]
 		
 		for tc in cases:
 			with self.subTest(desc=tc.desc):
 				res = pa_gen(tc.gen_name, tc.req_names, **tc.kwargs)
-				self.assertEqual(tc.exp, res)
+				self.check_param_aim(tc.exp, res)
 	
 	#def test_with_sink
 	
