5df6173746438bd211edaeef01e8f0b35402d909
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2022-01-24 07:44:43 +0100

    [CHANGE] use pa_gen for fitness.chromo_id

--- applications/discern_frequency/write_map_util.py
+++ applications/discern_frequency/write_map_util.py
@@ -53,9 +53,6 @@ def create_base(rep: IcecraftRep, chromo_bits: 16) -> Tuple[ParamAimMap, MetaEnt
 		),
 	]
 	
-	gene_desc = HDF5_DICT["rep.genes"]
-	const_desc = HDF5_DICT["rep.const"]
-	
 	write_map = {
 		"Measure.perform": [
 			pa_gen("fitness.st", ["driver_data"], comp_opt=9, shuffle=True),
@@ -194,8 +191,7 @@ def add_measure(write_map: ParamAimMap, metadata: MetaEntryMap, rep: IcecraftRep
 				itemgetter(0), attrgetter("fast_sum")]), comp_opt=9, shuffle=True),
 			ParamAim(["return"], "float64", "slow_sum", "fitness", as_attr=False, alter=partial(compose, funcs=[
 				itemgetter(0), attrgetter("slow_sum")]), comp_opt=9, shuffle=True),
-			ParamAim(["chromosome"], "uint64", "chromo_id", "fitness", as_attr=False, alter=partial(compose, funcs=[
-				itemgetter(0), attrgetter("identifier")]), comp_opt=9, shuffle=True),
+			pa_gen("fitness.chromo_id", ["chromosome"], comp_opt=9, shuffle=True),
 			pa_gen("carry_enable.values", ["return"], alter=partial(compose, funcs=[itemgetter(0),
 				attrgetter("carry_enable")]), shape=(len(list(rep.iter_carry_bits())), ), comp_opt=4),
 			ParamAim(["generation"], "uint64", "generation", "fitness", as_attr=False, comp_opt=9, shuffle=True),
