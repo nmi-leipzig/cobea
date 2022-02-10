056052a5efd7950ea1ece8000dd836aada01ef58
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2022-02-10 19:07:23 +0100

    [ADD] population initialization for predefined population

--- adapters/pop_init.py
+++ adapters/pop_init.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Iterable, List, Optional
 
 from domain.data_sink import DataSink
 from domain.interfaces import PopulationInit
@@ -8,8 +8,19 @@ from domain.request_model import RequestObject
 from domain.use_cases import RandomChromo
 
 class RandomPop(PopulationInit):
+	"""Initialize population randomly"""
 	def __init__(self, rep: Representation, uid_gen: UniqueID, prng: PRNG, data_sink: Optional[DataSink]=None) -> None:
 		self._init_uc = RandomChromo(prng, rep, uid_gen, data_sink)
 	
 	def init_pop(self, pop_size: int) -> List[Chromosome]:
 		return [self._init_uc(RequestObject()).chromosome for _ in range(pop_size)]
+
+class GivenPop(PopulationInit):
+	"""Initialize population with predefined population"""
+	def __init__(self, given_pop: Iterable[Chromosome]) -> None:
+		self._given = list(given_pop)	
+	
+	def init_pop(self, pop_size: int) -> List[Chromosome]:
+		if pop_size != len(self._given):
+			raise ValueError(f"requested {pop_size}, but given are {len(self._given)}")
+		return list(self._given)
--- tests/test_pop_init.py
+++ tests/test_pop_init.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from adapters.pop_init import RandomPop
+from adapters.pop_init import GivenPop, RandomPop
 from adapters.prng import BuiltInPRNG
 from adapters.simtar import SimtarRepGen
 from adapters.unique_id import SimpleUID
@@ -30,3 +30,31 @@ class RandomPopTest(TestCase):
 		self.assertEqual(7, len(res))
 		for chromo in res:
 			self.assertIsInstance(chromo, Chromosome)
+
+class GivenPopTest(TestCase):
+	def create_dut(self):
+		chromo_list = [
+			Chromosome(23, (1, 2, 3)),
+			Chromosome(42, (0, 7, 11)),
+			Chromosome(1, (12, 3, 1)),
+			Chromosome(274, (9, 1, 9)),
+		]
+		
+		dut = GivenPop(chromo_list)
+		
+		return chromo_list, dut
+	
+	def test_creation(self):
+		self.create_dut()
+	
+	def test_init_pop(self):
+		exp, dut = self.create_dut()
+		
+		with self.subTest(desc="normal call"):
+			res = dut.init_pop(len(exp))
+			
+			self.assertEqual(exp, res)
+		
+		for length in [0, len(exp)-1, len(exp)+1]:
+			with self.subTest(desc=f"invalid length {length}"), self.assertRaises(ValueError):
+				res = dut.init_pop(length)
