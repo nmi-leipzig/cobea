a3e2e5cb4ed952cfee7d5e081cf16b896a2fe28e
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2022-02-12 11:08:33 +0100

    [FIX] typo

--- applications/discern_frequency/misc.py
+++ applications/discern_frequency/misc.py
@@ -11,7 +11,7 @@ class DriverType(Enum):
 
 
 def ignore_same(x: list) -> Any:
-	"""raise IgnoreValue of first two elements are equal, else return the last
+	"""raise IgnoreValue if first two elements are equal, else return the last
 	
 	That way a third value can be rejected when two other values are identical
 	"""
